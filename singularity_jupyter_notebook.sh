6659e150c1db80188fe96162b3849b0d6351acd0
Mark Saddler <msaddler@ranvier.eth.cluster>
2019-06-16 15:55:20 -0400

    singularity jupyter notebook job

---
+++ singularity_jupyter_notebook.sh
@@ -0,0 +1,12 @@
+user=$(whoami)
+
+singularity exec --nv \
+-B /home \
+-B /om \
+-B /om2 \
+-B /om2/user/msaddler/python-packages:/python-packages \
+-B /om/user/msaddler/data_pitchnet:/data \
+-B /om2/user/$user/pitchnet/saved_models:/saved_models \
+-B /om2/user/$user/pitchnet/ibmHearingAid:/code_location \
+/om/user/francl/ibm_hearing_aid/tfv1.13_openmind.simg \
+/om2/user/msaddler/jupyter_notebook_job.sh
