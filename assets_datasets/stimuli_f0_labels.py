38810488acb35214b1aa0d9989becfc5d39c7a96
Mark Saddler <msaddler@ranvier.eth.cluster>
2019-06-27 16:55:13 -0400

    regression training on log-normalized f0 values

--- assets_datasets/stimuli_f0_labels.py
+++ assets_datasets/stimuli_f0_labels.py
@@ -43,8 +43,21 @@ def label_to_f0(f0_labels, bins, strict_bin_minimum=True):
     return f0_values
 
 
-def add_f0_label_to_hdf5(hdf5_filename, f0_key='f0', f0_label_key='f0_label',
-                         f0_label_dtype=np.int64, f0_bin_kwargs={}):
+def normalize_f0(f0_values, f0_min=80., f0_max=1e3, log_scale=True):
+    '''
+    Helper function to convert f0_values to [0, 1] range for regression.
+    '''
+    assert np.all(f0_values >= f0_min), "f0_values must be greater than or equal to f0_min"
+    assert np.all(f0_values <= f0_max), "f0_values must be less than or equal to f0_max"
+    if log_scale:
+        f0_normalized = np.log(f0_values/f0_min) / np.log(f0_max/f0_min)
+    else:
+        f0_normalized = (f0_values - f0_min) / (f0_max - f0_min)
+    return f0_normalized
+
+
+def add_f0_label_to_hdf5(hdf5_filename, f0_key='f0', f0_label_key='f0_label', f0_normal_key='f0_lognormal',
+                         f0_label_dtype=np.int64, f0_bin_kwargs={}, f0_normalization_kwargs={}):
     '''
     Function adds or recomputes f0 labels dataset within specified hdf5 file.
     
@@ -53,8 +66,10 @@ def add_f0_label_to_hdf5(hdf5_filename, f0_key='f0', f0_label_key='f0_label',
     hdf5_filename (str)
     f0_key (str): hdf5 path to f0 values (hdf5 dataset should have shape (N,))
     f0_label_key (str): hdf5 path to f0 labels (this dataset will be added or overwritten)
+    f0_normal_key (str): hdf5 path to normalized f0 values (this dataset will be added or overwritten)
     f0_label_dtype (np.dtype): datatype for f0 label dataset
     f0_bin_kwargs (dict): kwargs for `get_f0_bins()` (parameters for computing f0 label bins)
+    f0_normalization_kwargs (dict): kwargs for `f0_normalization_kwargs()` (parameters for normalizing f0 values)
     '''
     bins = get_f0_bins(**f0_bin_kwargs)
     print('[ADDING F0 LABELS]: {}'.format(hdf5_filename))
@@ -62,12 +77,21 @@ def add_f0_label_to_hdf5(hdf5_filename, f0_key='f0', f0_label_key='f0_label',
     hdf5_f = h5py.File(hdf5_filename, 'r+')
     f0_values = hdf5_f[f0_key][:]
     f0_labels = f0_to_label(f0_values, bins)
+    f0_normal = normalize_f0(f0_values, **f0_normalization_kwargs)
+    # Write the f0 bin labels to hdf5 file
     if f0_label_key in hdf5_f:
         print('[OVERWRITING DATASET]: {}'.format(f0_label_key))
         hdf5_f[f0_label_key][:] = f0_labels
     else:
         print('[INITIALIZING DATASET]: {}'.format(f0_label_key))
         hdf5_f.create_dataset(f0_label_key, f0_labels.shape, dtype=f0_label_dtype, data=f0_labels)
+    # Write the f0 normalized values to hdf5 file
+    if f0_normal_key in hdf5_f:
+        print('[OVERWRITING DATASET]: {}'.format(f0_normal_key))
+        hdf5_f[f0_normal_key][:] = f0_normal
+    else:
+        print('[INITIALIZING DATASET]: {}'.format(f0_normal_key))
+        hdf5_f.create_dataset(f0_normal_key, f0_normal.shape, dtype=f0_values.dtype, data=f0_normal)
     hdf5_f.close()
     print('[END]: {}'.format(hdf5_filename))
 
@@ -79,5 +103,5 @@ if __name__ == "__main__":
     hdf5_fn_list = sorted(glob.glob(hdf5_fn_regex))
     for hdf5_filename in hdf5_fn_list:
         print('|===| {} |===|'.format(hdf5_filename))
-        add_f0_label_to_hdf5(hdf5_filename, f0_key='f0', f0_label_key='f0_label',
-                             f0_label_dtype=np.int64, f0_bin_kwargs={})
+        add_f0_label_to_hdf5(hdf5_filename, f0_key='f0', f0_label_key='f0_label', f0_normal_key='f0_lognormal',
+                             f0_label_dtype=np.int64, f0_bin_kwargs={}, f0_normalization_kwargs={})
