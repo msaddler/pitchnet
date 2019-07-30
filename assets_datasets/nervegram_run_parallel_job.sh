bc0f346af52e74794e9d49b0e36edd5d0acc7da2
Mark Saddler <msaddler@mit.edu>
2019-07-30 15:08:52 -0400

    generating moving-filter bernox2005-like stimuli (legacy)

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -13,8 +13,8 @@
 #SBATCH --qos=use-everything
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-source_regex='/om/scratch/Wed/msaddler/data_pitchnet/PND_v01/noise_JWSS_snr_pos10pos30/PND_sr32000_v01_*.hdf5'
-dest_filename='/om/scratch/Wed/msaddler/data_pitchnet/PND_v01/noise_JWSS_snr_pos10pos30/cf100_species002_spont070/bez2018meanrates.hdf5'
+source_regex='/om/scratch/Wed/msaddler/data_pitchnet/PND_v01/noise_JWSS_snr_neg10pos03/augment_halfbandpass/PND_sr32000_v01_*.hdf5'
+dest_filename='/om/scratch/Wed/msaddler/data_pitchnet/PND_v01/noise_JWSS_snr_neg10pos03/augment_halfbandpass/cf100_species002_spont070/bez2018meanrates.hdf5'
 jobs_per_source_file=5
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
--- assets_datasets/stimuli_generate_synthetic_tones.py
+++ assets_datasets/stimuli_generate_synthetic_tones.py
@@ -3,11 +3,13 @@ import os
 import numpy as np
 import h5py
 import scipy.signal
+import itertools
+import pdb
 
 import stimuli_util
 
-sys.path.append('/om2/user/msaddler/python-packages/bez2018model')
-from bez2018model_run_hdf5_dataset import initialize_hdf5_file, write_example_to_hdf5
+sys.path.append('/om4/group/mcdermott/user/msaddler/pitchnet_dataset/pitchnetDataset/pitchnetDataset')
+from dataset_util import initialize_hdf5_file, write_example_to_hdf5
 
 
 def lowpass_complex_tone(f0, fs, dur, attenuation_start=1000., attenuation_slope=0.,
@@ -113,8 +115,7 @@ def generate_lowpass_complex_tone_dataset(hdf5_filename, N, fs=32e3, dur=0.150,
                     data_key_pair_list.append((k, k))
             initialize_hdf5_file(hdf5_filename, N, data_dict, file_mode='w',
                                  data_key_pair_list=data_key_pair_list,
-                                 config_key_pair_list=config_key_pair_list,
-                                 dtype=np.float32, cast_data=False, cast_config=False)
+                                 config_key_pair_list=config_key_pair_list)
             hdf5_f = h5py.File(hdf5_filename, 'r+')
         # Write each data_dict to hdf5 file
         write_example_to_hdf5(hdf5_f, data_dict, itrN, data_key_pair_list=data_key_pair_list)
@@ -191,6 +192,170 @@ def bernox2005_bandpass_complex_tone(f0, fs, dur, frequency_response_in_dB=None,
                                        amplitudes=amplitudes, **kwargs_complex_tone)
     audible_harmonic_numbers = harmonic_numbers[harmonic_dBSPL >= threshold_dBSPL]
     return signal, audible_harmonic_numbers
+
+
+def shift_bandpass_filter_frequency_response(desired_fl, desired_fl_gain_in_dB,
+                                             fs=32000, unshifted_passband=None,
+                                             frequency_response_in_dB=None):
+    '''
+    '''
+    assert frequency_response_in_dB is not None, "`frequency_response_in_dB` must be specified"
+    # Compute the `desired_fl_gain_in_dB` passband of the unshifted filter
+    if unshifted_passband is None:
+        freq_vector = np.arange(0, fs/2)
+        gain_vector = frequency_response_in_dB(freq_vector)
+        passed_freqs = freq_vector[gain_vector > desired_fl_gain_in_dB]
+        unshifted_passband = [np.min(passed_freqs), np.max(passed_freqs)]
+    # Compute offset between unshifted and desired filter
+    fl_offset = desired_fl - unshifted_passband[0]
+    # Return frequency response function of shifted filter
+    def shifted_frequency_response_in_dB(freqs):
+        shifted_freqs = freqs - fl_offset
+        shifted_freqs[shifted_freqs  < 0.] = 0.
+        shifted_freqs[shifted_freqs  > fs/2] = fs/2
+        return frequency_response_in_dB(shifted_freqs)
+    return shifted_frequency_response_in_dB
+
+
+def bernoxMovingFilter_bandpass_complex_tone(f0, fs, dur, low_harm, frequency_response_in_dB=None,
+                                             threshold_dBSPL=33.3, component_dBSL=15.,
+                                             **kwargs_complex_tone):
+    '''
+    '''
+    # Define the harmonic stack
+    harmonics = np.arange(f0, fs/2, f0)
+    harmonic_numbers = np.rint(harmonics / f0).astype(int)
+    # Shift the baseline frequency response function to the desired low_harm number and gain
+    desired_fl = f0 * low_harm
+    desired_fl_gain_in_dB = -1 * component_dBSL
+    shifted_frequency_response_in_dB = shift_bandpass_filter_frequency_response(
+        desired_fl, desired_fl_gain_in_dB, fs=fs, unshifted_passband=None,
+        frequency_response_in_dB=frequency_response_in_dB)
+    # Generate harmonic complex tone with the shifted frequency response function
+    harmonic_dBSPL = threshold_dBSPL + component_dBSL + shifted_frequency_response_in_dB(harmonics)
+    amplitudes = 20e-6 * np.power(10, (harmonic_dBSPL/20))
+    signal = stimuli_util.complex_tone(f0, fs, dur, harmonic_numbers=harmonic_numbers,
+                                       amplitudes=amplitudes, **kwargs_complex_tone)
+    audible_harmonic_numbers = harmonic_numbers[harmonic_dBSPL >= threshold_dBSPL]
+    # Assert that the lowest audible harmonic number is indeed the specified low_harm number
+    if not int(np.min(audible_harmonic_numbers)) == low_harm:
+        print(f0, low_harm, audible_harmonic_numbers)
+        pdb.set_trace()
+    assert int(np.min(audible_harmonic_numbers)) == low_harm
+    return signal, audible_harmonic_numbers
+
+
+def generate_bernoxMovingFilter_dataset(hdf5_filename, fs=32e3, dur=0.150, f0_min=100., f0_max=300.,
+                                        step_size_in_octaves=4/768, phase_modes=['sine', 'rand'],
+                                        low_harm_min=1, low_harm_max=30,
+                                        highpass_filter_cutoff=2.5e3, lowpass_filter_cutoff=3.5e3,
+                                        filter_order=4, threshold_dBSPL=33.3, component_dBSL=15.,
+                                        noise_dBHzSPL=15., noise_attenuation_start=600.,
+                                        noise_attenuation_slope=2, disp_step=100):
+    '''
+    Main routine for generating modified form of Bernstein and Oxenham (2005, JASA)
+    diagnostic dataset. The bandpass filter from Bernstein and Oxenham (2005, JASA)
+    is translated linearly along the frequency axis to manipulate the lowest audible
+    harmonic number in the resulting stimuli.
+    
+    Args
+    ----
+    hdf5_filename (str):
+    fs (int): sampling rate (Hz)
+    dur (float): duration of tone (s)
+    f0_min (float):
+    f0_max (float):
+    step_size_in_octaves (float):
+    phase_modes (list):
+    low_harm_min (int):
+    low_harm_max (int):
+    highpass_filter_cutoff (float):
+    lowpass_filter_cutoff (float):
+    filter_order (int):
+    threshold_dBSPL (float):
+    component_dBSL (float):
+    noise_dBHzSPL (float):
+    noise_attenuation_start (float):
+    noise_attenuation_slope (float):
+    disp_step (int):
+    '''
+    # Define encoding / decoding dictionaries for phase_mode
+    phase_mode_encoding = {'sine':0, 'rand':1, 'sch':2, 'cos':3, 'alt':4}
+    phase_mode_decoding = {0:'sine', 1:'rand', 2:'sch', 3:'cos', 4:'alt'}
+    # Determine lists of unique low harm numbers, f0s, and phase values
+    unique_low_harm_numbers = np.arange(low_harm_min, low_harm_max + 1)
+    unique_f0s = np.arange(0, np.log2(f0_max / f0_min), step_size_in_octaves)
+    unique_f0s = f0_min * (np.power(2, unique_f0s))
+    unique_phase_modes = np.array([phase_mode_encoding[p] for p in phase_modes])
+    # Generate parameter lists to iterate over (all combinations of low_harm, f0, and phase_mode)
+    (list_lh, list_f0, list_ph) = ([], [], [])
+    for (lh, f0, ph) in itertools.product(unique_low_harm_numbers, unique_f0s, unique_phase_modes):
+        list_lh.append(lh)
+        list_f0.append(f0)
+        list_ph.append(ph)
+    N = len(list_lh)
+    # Calculate the "unshifted" bandpass filter frequency response function to use as a baseline
+    frequency_response_in_dB = get_bandpass_filter_frequency_response(highpass_filter_cutoff,
+                                                                      lowpass_filter_cutoff,
+                                                                      fs=fs, order=filter_order)
+    # Prepare data_dict and config_key_pair_list for hdf5 filewriting
+    data_dict = {
+        'config_tone/fs': fs,
+        'config_tone/dur': dur,
+        'config_tone/f0_min': f0_min,
+        'config_tone/f0_max': f0_max,
+        'config_tone/step_size_in_octaves': step_size_in_octaves,
+        'config_tone/low_harm_min': low_harm_min,
+        'config_tone/low_harm_max': low_harm_max,
+        'config_tone/unshifted_highpass_filter_cutoff': highpass_filter_cutoff,
+        'config_tone/unshifted_lowpass_filter_cutoff': lowpass_filter_cutoff,
+        'config_tone/unshifted_filter_order': filter_order,
+        'config_tone/threshold_dBSPL': threshold_dBSPL,
+        'config_tone/component_dBSL': component_dBSL,
+        'config_tone/noise_dBHzSPL': noise_dBHzSPL,
+        'config_tone/noise_attenuation_start': noise_attenuation_start,
+        'config_tone/noise_attenuation_slope': noise_attenuation_slope,
+    }
+    config_key_pair_list = [(k, k) for k in data_dict.keys()]
+    data_key_pair_list = [] # Will be populated right before initializing hdf5 file
+    # Main loop to generate the bandpass filtered tones
+    for itrN in range(0, N):
+        
+        signal, audible_harmonic_numbers = bernoxMovingFilter_bandpass_complex_tone(
+            list_f0[itrN], fs, dur, list_lh[itrN],
+            frequency_response_in_dB=frequency_response_in_dB,
+            threshold_dBSPL=threshold_dBSPL,
+            component_dBSL=component_dBSL,
+            phase_mode=phase_mode_decoding[list_ph[itrN]])
+        
+        noise = stimuli_util.modified_uniform_masking_noise(fs, dur, dBHzSPL=noise_dBHzSPL,
+                                                            attenuation_start=noise_attenuation_start,
+                                                            attenuation_slope=noise_attenuation_slope)
+        
+        tone_in_noise = signal + noise
+        data_dict['tone_in_noise'] = tone_in_noise.astype(np.float32)
+        data_dict['f0'] = list_f0[itrN]
+        data_dict['phase_mode'] = int(list_ph[itrN])
+        data_dict['low_harm'] = int(np.min(audible_harmonic_numbers))
+        data_dict['upp_harm'] = int(np.max(audible_harmonic_numbers))
+        
+        # Initialize the hdf5 file on the first iteration
+        if itrN == 0:
+            print('[INITIALIZING]: {}'.format(hdf5_filename))
+            for k in data_dict.keys():
+                if not (k, k) in config_key_pair_list:
+                    data_key_pair_list.append((k, k))
+            initialize_hdf5_file(hdf5_filename, N, data_dict, file_mode='w',
+                                 data_key_pair_list=data_key_pair_list,
+                                 config_key_pair_list=config_key_pair_list)
+            hdf5_f = h5py.File(hdf5_filename, 'r+')
+        # Write each data_dict to hdf5 file
+        write_example_to_hdf5(hdf5_f, data_dict, itrN, data_key_pair_list=data_key_pair_list)
+        if itrN % disp_step == 0:
+            print('... signal {} of {}'.format(itrN, N))
+    # Close hdf5 file
+    hdf5_f.close()
+    print('[END]: {}'.format(hdf5_filename))
 
 
 def generate_bernox2005_dataset(hdf5_filename, fs=32e3, dur=0.150, f0_min=80., f0_max=1e3,
@@ -281,8 +446,7 @@ def generate_bernox2005_dataset(hdf5_filename, fs=32e3, dur=0.150, f0_min=80., f
                     data_key_pair_list.append((k, k))
             initialize_hdf5_file(hdf5_filename, N, data_dict, file_mode='w',
                                  data_key_pair_list=data_key_pair_list,
-                                 config_key_pair_list=config_key_pair_list,
-                                 dtype=np.float32, cast_data=False, cast_config=False)
+                                 config_key_pair_list=config_key_pair_list)
             hdf5_f = h5py.File(hdf5_filename, 'r+')
         # Write each data_dict to hdf5 file
         write_example_to_hdf5(hdf5_f, data_dict, itrN, data_key_pair_list=data_key_pair_list)
@@ -364,8 +528,7 @@ def generate_bandpass_complex_tone_dataset(hdf5_filename, N, fs=32e3, dur=0.150,
                     data_key_pair_list.append((k, k))
             initialize_hdf5_file(hdf5_filename, N, data_dict, file_mode='w',
                                  data_key_pair_list=data_key_pair_list,
-                                 config_key_pair_list=config_key_pair_list,
-                                 dtype=np.float32, cast_data=False, cast_config=False)
+                                 config_key_pair_list=config_key_pair_list)
             hdf5_f = h5py.File(hdf5_filename, 'r+')
         # Write each data_dict to hdf5 file
         write_example_to_hdf5(hdf5_f, data_dict, itrN, data_key_pair_list=data_key_pair_list)
@@ -381,6 +544,14 @@ if __name__ == "__main__":
     assert len(sys.argv) == 3, "scipt usage: python <script_name> <hdf5_filename> <N>"
     hdf5_filename = str(sys.argv[1])
     N = int(sys.argv[2])
-    generate_bandpass_complex_tone_dataset(hdf5_filename, N)
+#     generate_bandpass_complex_tone_dataset(hdf5_filename, N)
 #     generate_lowpass_complex_tone_dataset(hdf5_filename, N)
 #     generate_bernox2005_dataset(hdf5_filename, highpass_filter_cutoff=5e3, lowpass_filter_cutoff=7e3)
+
+    generate_bernoxMovingFilter_dataset(hdf5_filename, fs=32e3, dur=0.150, f0_min=100., f0_max=300.,
+                                        step_size_in_octaves=1/768, phase_modes=['sine', 'rand'],
+                                        low_harm_min=1, low_harm_max=30,
+                                        highpass_filter_cutoff=2.5e3, lowpass_filter_cutoff=3.5e3,
+                                        filter_order=4, threshold_dBSPL=33.3, component_dBSL=15.,
+                                        noise_dBHzSPL=15., noise_attenuation_start=600.,
+                                        noise_attenuation_slope=2, disp_step=100)
