636978473ac342c6ec52877a02d9c639190c7b37
Mark Saddler <msaddler@mit.edu>
2019-08-15 23:56:53 -0400

    generating f0 restricted extended diagnostics

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -8,14 +8,14 @@
 #SBATCH --time=0-15:00:00
 #SBATCH --time-min=0-9:00:00
 #SBATCH --exclude=node[001-029]
-#SBATCH --array=21-24,200
+#SBATCH --array=0-23
 #SBATCH --qos=mcdermott
 ##SBATCH --qos=use-everything
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-source_regex='/om/scratch/Sun/msaddler/data_pitchnet/PND_v03/noise_JWSS_snr_neg10pos03/augment_halfbandpass/PND_sr32000_v03*.hdf5'
-dest_filename='/om/scratch/Sun/msaddler/data_pitchnet/PND_v03/noise_JWSS_snr_neg10pos03/augment_halfbandpass/cf100_species002_spont070/bez2018meanrates.hdf5'
-jobs_per_source_file=3
+source_regex='/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz/*.hdf5'
+dest_filename='/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz/cf100_species002_spont070/bez2018meanrates.hdf5'
+jobs_per_source_file=24
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 
