19b990f74d9513b663d73507596f56d8a6b7b4e6
Mark Saddler <msaddler@mit.edu>
2019-08-20 23:32:19 -0400

    generating highSNR, speechOnly, instOnly dataset manipulations

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -5,17 +5,18 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4000
 #SBATCH --nodes=1
-#SBATCH --time=0-16:00:00
-#SBATCH --time-min=0-10:00:00
+#SBATCH --time=0-36:00:00
+#SBATCH --time-min=0-30:00:00
 #SBATCH --exclude=node[001-030,069]
-#SBATCH --array=0-49
+#SBATCH --array=0-299
 ##SBATCH --partition=mcdermott
-##SBATCH --partition=use-everything
+#SBATCH --partition=use-everything
+#SBATCH --requeue
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-source_regex='/om/user/msaddler/data_pitchnet/moore1985/Moore1985_MistunedHarmonics_v00/*.hdf5'
-dest_filename='/om/user/msaddler/data_pitchnet/moore1985/Moore1985_MistunedHarmonics_v00/cf100_species002_spont070/bez2018meanrates.hdf5'
-jobs_per_source_file=50
+source_regex='/om/scratch/Fri/msaddler/data_pitchnet/PND_v06/noise_JWSS_snr_neg10pos03/*.hdf5'
+dest_filename='/om/scratch/Fri/msaddler/data_pitchnet/PND_v06/noise_JWSS_snr_neg10pos03/cf100_species002_spont070/bez2018meanrates.hdf5'
+jobs_per_source_file=3
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 
