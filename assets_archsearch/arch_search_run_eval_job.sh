a1f3d3067c28ac8c11f610bac5101cc5564abee6
Mark Saddler <msaddler@mit.edu>
2019-08-29 13:53:41 -0400

    transposed tones experiment with arch search networks

--- assets_archsearch/arch_search_run_eval_job.sh
+++ assets_archsearch/arch_search_run_eval_job.sh
@@ -8,9 +8,10 @@
 #SBATCH --gres=gpu:GEFORCEGTX1080TI:1
 ####SBATCH --gres=gpu:GEFORCERTX2080:1
 ####SBATCH --gres=gpu:1 --constraint=high-capacity
-#SBATCH --time=0-2:00:00
-#SBATCH --array=102,104,107,108,112,139,141,143,625
-#SBATCH --partition=mcdermott
+#SBATCH --time=0-4:00:00
+#SBATCH --time-min=0-2:00:00
+#SBATCH --array=251-749
+##SBATCH --partition=mcdermott
 ##SBATCH --partition=use-everything
 #SBATCH --requeue
 
@@ -19,6 +20,23 @@ job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 OUTPUT_DIR=$(printf "/saved_models/arch_search_v00/arch_%04d" ${job_idx})
 echo $(hostname)
 
+# singularity exec --nv \
+# -B /home \
+# -B /nobackup \
+# -B /om \
+# -B /om2 \
+# -B /om4 \
+# -B /om2/user/msaddler/python-packages:/python-packages \
+# -B /om/scratch/Mon/msaddler/data_pitchnet:/data \
+# -B /om/scratch/Mon/msaddler/pitchnet/saved_models:/saved_models \
+# -B /om2/user/msaddler/pitchnet/ibmHearingAid:/code_location \
+# /om/user/francl/ibm_hearing_aid/tfv1.13_openmind.simg \
+# python /om2/user/msaddler/pitchnet/assets_archsearch/arch_search_run_eval.py \
+# -de '/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/cf100_species002_spont070/*.tfrecords' \
+# -efn 'EVAL_bernox2005_FixedFilter_bestckpt.json' \
+# -o "${OUTPUT_DIR}"
+
+
 singularity exec --nv \
 -B /home \
 -B /nobackup \
@@ -26,11 +44,62 @@ singularity exec --nv \
 -B /om2 \
 -B /om4 \
 -B /om2/user/msaddler/python-packages:/python-packages \
--B /om/scratch/Fri/msaddler/data_pitchnet:/data \
--B /om/scratch/Fri/msaddler/pitchnet/saved_models:/saved_models \
+-B /om/scratch/Mon/msaddler/data_pitchnet:/data \
+-B /om/scratch/Mon/msaddler/pitchnet/saved_models:/saved_models \
 -B /om2/user/msaddler/pitchnet/ibmHearingAid:/code_location \
 /om/user/francl/ibm_hearing_aid/tfv1.13_openmind.simg \
 python /om2/user/msaddler/pitchnet/assets_archsearch/arch_search_run_eval.py \
--de '/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/cf100_species002_spont070/*.tfrecords' \
--efn 'EVAL_bernox2005_FixedFilter_bestckpt.json' \
+-de '/om/user/msaddler/data_pitchnet/mooremoore2003/MooreMoore2003_frequencyShiftedComplexes_f0_080to480Hz/cf100_species002_spont070/*.tfrecords' \
+-efn 'EVAL_mooremoore2003_080to480Hz_bestckpt.json' \
+-o "${OUTPUT_DIR}"
+
+
+singularity exec --nv \
+-B /home \
+-B /nobackup \
+-B /om \
+-B /om2 \
+-B /om4 \
+-B /om2/user/msaddler/python-packages:/python-packages \
+-B /om/scratch/Mon/msaddler/data_pitchnet:/data \
+-B /om/scratch/Mon/msaddler/pitchnet/saved_models:/saved_models \
+-B /om2/user/msaddler/pitchnet/ibmHearingAid:/code_location \
+/om/user/francl/ibm_hearing_aid/tfv1.13_openmind.simg \
+python /om2/user/msaddler/pitchnet/assets_archsearch/arch_search_run_eval.py \
+-de '/om/user/msaddler/data_pitchnet/moore1985/Moore1985_MistunedHarmonics_v00/cf100_species002_spont070/*.tfrecords' \
+-efn 'EVAL_MistunedHarm_v00_bestckpt.json' \
+-o "${OUTPUT_DIR}"
+
+
+singularity exec --nv \
+-B /home \
+-B /nobackup \
+-B /om \
+-B /om2 \
+-B /om4 \
+-B /om2/user/msaddler/python-packages:/python-packages \
+-B /om/scratch/Mon/msaddler/data_pitchnet:/data \
+-B /om/scratch/Mon/msaddler/pitchnet/saved_models:/saved_models \
+-B /om2/user/msaddler/pitchnet/ibmHearingAid:/code_location \
+/om/user/francl/ibm_hearing_aid/tfv1.13_openmind.simg \
+python /om2/user/msaddler/pitchnet/assets_archsearch/arch_search_run_eval.py \
+-de '/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz/cf100_species002_spont070/*.tfrecords' \
+-efn 'EVAL_oxenham2004_080to320Hz_bestckpt.json' \
+-o "${OUTPUT_DIR}"
+
+
+singularity exec --nv \
+-B /home \
+-B /nobackup \
+-B /om \
+-B /om2 \
+-B /om4 \
+-B /om2/user/msaddler/python-packages:/python-packages \
+-B /om/scratch/Mon/msaddler/data_pitchnet:/data \
+-B /om/scratch/Mon/msaddler/pitchnet/saved_models:/saved_models \
+-B /om2/user/msaddler/pitchnet/ibmHearingAid:/code_location \
+/om/user/francl/ibm_hearing_aid/tfv1.13_openmind.simg \
+python /om2/user/msaddler/pitchnet/assets_archsearch/arch_search_run_eval.py \
+-de '/om/user/msaddler/data_pitchnet/shackcarl1994/AltPhase_v01_f0min080_f0max320/cf100_species002_spont070/*.tfrecords' \
+-efn 'EVAL_AltPhase_v01_bestckpt.json' \
 -o "${OUTPUT_DIR}"
--- assets_psychophysics/f0dl_transposed_tones.py
+++ assets_psychophysics/f0dl_transposed_tones.py
@@ -2,6 +2,8 @@ import sys
 import os
 import json
 import numpy as np
+import glob
+import argparse
 import pdb
 import scipy.optimize
 import scipy.stats
@@ -15,7 +17,7 @@ def run_f0dl_experiment(json_fn, max_pct_diff=6, noise_stdev=1e-12, bin_width=5e
                         threshold_value=0.707, use_empirical_f0dl_if_possible=False,
                         f0_label_true_key='f0_label:labels_true', f0_label_pred_key='f0_label:labels_pred',
                         kwargs_f0_bins={}, kwargs_f0_octave={}, kwargs_f0_normalization={},
-                        f0_ref_min=80.0, f0_ref_max=320.0, f0_ref_n_step=5,
+                        f0_ref_min=90.0, f0_ref_max=300.0, f0_ref_n_step=5,
                         metadata_key_list=['f_carrier', 'f_envelope', 'f0']):
     '''
     '''
@@ -77,3 +79,62 @@ def run_f0dl_experiment(json_fn, max_pct_diff=6, noise_stdev=1e-12, bin_width=5e
             itr0 = itr0 + 1
     # Return dictionary of psychophysical experiment results
     return results_dict
+
+
+def main(json_eval_fn, json_results_dict_fn=None, save_results_to_file=False,
+         max_pct_diff=6, noise_stdev=1e-12, bin_width=5e-2, mu=0.0,
+         threshold_value=0.707, use_empirical_f0dl_if_possible=False,
+         f0_label_true_key='f0_label:labels_true', f0_label_pred_key='f0_label:labels_pred',
+         kwargs_f0_bins={}, kwargs_f0_octave={}, kwargs_f0_normalization={},
+         f0_ref_min=90.0, f0_ref_max=300.0, f0_ref_n_step=5,
+         metadata_key_list=['f_carrier', 'f_envelope', 'f0']):
+    '''
+    '''
+    # Run the Oxenham et al. (2004) transposed tones F0DL experiment; results stored in results_dict
+    results_dict = run_f0dl_experiment(json_eval_fn,
+                                       max_pct_diff=max_pct_diff, noise_stdev=noise_stdev,
+                                       bin_width=bin_width, mu=mu, threshold_value=threshold_value,
+                                       use_empirical_f0dl_if_possible=use_empirical_f0dl_if_possible,
+                                       f0_label_true_key=f0_label_true_key, f0_label_pred_key=f0_label_pred_key,
+                                       kwargs_f0_bins=kwargs_f0_bins,
+                                       kwargs_f0_octave=kwargs_f0_octave,
+                                       kwargs_f0_normalization=kwargs_f0_normalization,
+                                       f0_ref_min=f0_ref_min,
+                                       f0_ref_max=f0_ref_max,
+                                       f0_ref_n_step=f0_ref_n_step,
+                                       metadata_key_list=metadata_key_list)
+    results_dict['json_eval_fn'] = json_eval_fn
+    # If specified, save results_dict to file
+    if save_results_to_file:
+        # Check filename for results_dict
+        if json_results_dict_fn is None:
+            json_results_dict_fn = json_eval_fn.replace('.json', '_results_dict.json')
+        assert not json_results_dict_fn == json_eval_fn, "json_results_dict_fn must not overwrite json_eval_fn"
+        # Define helper class to JSON serialize the results_dict
+        class NumpyEncoder(json.JSONEncoder):
+            def default(self, obj):
+                if isinstance(obj, np.ndarray): return obj.tolist()
+                if isinstance(obj, np.int64): return int(obj)  
+                return json.JSONEncoder.default(self, obj)
+        # Write results_dict to json_results_dict_fn
+        with open(json_results_dict_fn, 'w') as f: json.dump(results_dict, f, cls=NumpyEncoder)
+        print('[END] wrote results_dict to {}'.format(json_results_dict_fn))
+    return results_dict
+
+
+if __name__ == "__main__":
+    '''
+    '''
+    parser = argparse.ArgumentParser(description="run Oxenham et al. (2004) transposed tones F0DL experiment")
+    parser.add_argument('-r', '--regex_json_eval_fn', type=str, default=None,
+                        help='regex that globs')
+    parser.add_argument('-j', '--job_idx', type=int, default=None,
+                        help='job index used to name current output directory')
+    parsed_args_dict = vars(parser.parse_args())
+    assert parsed_args_dict['regex_json_eval_fn'] is not None, "regex_json_eval_fn is a required argument"
+    assert parsed_args_dict['job_idx'] is not None, "job_idx is a required argument"
+    list_json_eval_fn = sorted(glob.glob(parsed_args_dict['regex_json_eval_fn']))
+    json_eval_fn = list_json_eval_fn[parsed_args_dict['job_idx']]
+    print('Processing file {} of {}'.format(parsed_args_dict['job_idx'], len(list_json_eval_fn)))
+    print('Processing file: {}'.format(json_eval_fn))
+    main(json_eval_fn, save_results_to_file=True)
