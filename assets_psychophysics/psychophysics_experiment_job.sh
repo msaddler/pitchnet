0a96640fbfb757b0d3b25a84a5f4dfd9ed11488e
Mark Saddler <msaddler@mit.edu>
2019-08-29 17:20:37 -0400

    running transposed tones on arch search networks

---
+++ assets_psychophysics/psychophysics_experiment_job.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --job-name=psychophysics
+#SBATCH --out="slurm-%A_%a.out"
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=4000
+#SBATCH --time=0-00:20:00
+#SBATCH --array=0-548
+##SBATCH --partition=mcdermott
+##SBATCH --partition=use-everything
+#SBATCH --requeue
+
+offset=0
+job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
+echo $(hostname)
+
+# python /om2/user/msaddler/pitchnet/assets_psychophysics/f0dl_bernox.py \
+# -r '/om/scratch/Mon/msaddler/pitchnet/saved_models/arch_search_v00/*/EVAL_bernox2005_FixedFilter_bestckpt.json' \
+# -j ${job_idx}
+
+python /om2/user/msaddler/pitchnet/assets_psychophysics/f0dl_transposed_tones.py \
+-r '/om/scratch/Mon/msaddler/pitchnet/saved_models/arch_search_v00/*/EVAL_oxenham2004_080to320Hz_bestckpt.json' \
+-j ${job_idx}
