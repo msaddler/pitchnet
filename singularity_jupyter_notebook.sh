0b8eebece9b30f2449e2303ea4313d965f77926f
Mark Saddler <msaddler@mit.edu>
2019-09-25 12:44:17 -0400

    alt phase summary figure

--- singularity_jupyter_notebook.sh
+++ singularity_jupyter_notebook.sh
@@ -5,10 +5,9 @@ singularity exec --nv \
 -B /om \
 -B /nobackup \
 -B /om2 \
--B /om4 \
 -B /om2/user/msaddler/python-packages:/python-packages \
 -B /om/user/msaddler/data_pitchnet:/data \
 -B /om2/user/$user/pitchnet/saved_models:/saved_models \
 -B /om2/user/$user/pitchnet/ibmHearingAid:/code_location \
-/om/user/francl/ibm_hearing_aid/tfv1.13_openmind.simg \
+/om2/user/msaddler/singularity-images/tfv1.13_unet.simg \
 /om2/user/msaddler/jupyter_notebook_job.sh
