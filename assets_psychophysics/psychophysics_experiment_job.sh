b335b693c8976e6677f2c2126ff4a14641aaa1c1
Mark Saddler <msaddler@mit.edu>
2019-09-26 11:29:55 -0400

    arch_0628 highpass unnatural sound statistics psychophysics

--- assets_psychophysics/psychophysics_experiment_job.sh
+++ assets_psychophysics/psychophysics_experiment_job.sh
@@ -15,7 +15,7 @@ offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 echo $(hostname)
 
-OUTDIR_REGEX='/om2/user/msaddler/pitchnet/saved_models/PND_v04_TLAS_halflowpassnoise_v02_classification*'
+OUTDIR_REGEX='/om2/user/msaddler/pitchnet/saved_models/arch_0628/PND_v04_JWSS_highpass_v00_classification*'
 
 # source activate mdlab
 
