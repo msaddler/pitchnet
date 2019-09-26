d5b069a29069e3aa33dcc23b3d911d97d4cfc8d5
Mark Saddler <msaddler@mit.edu>
2019-09-26 16:32:05 -0400

    evaluating networks on lowpass-filtered nervegrams

--- pitchnet_evaluate_best_job.sh
+++ pitchnet_evaluate_best_job.sh
@@ -3,15 +3,16 @@
 #SBATCH --job-name=pitchnet_eval
 #SBATCH --out="/om2/user/msaddler/pitchnet/slurm-%A_%a.out"
 #SBATCH --gres=gpu:GEFORCEGTX1080TI:1
+##SBATCH --gres=gpu:titan-x:1
 #SBATCH --mem=8000
 #SBATCH --cpus-per-task=4
 #SBATCH --time=0-2:00:00
-##SBATCH --exclude=node063
+#SBATCH --exclude=node063
 #SBATCH --partition=mcdermott
 #SBATCH --array=0-2
 
-OUTDIR='/saved_models/arch_0628/PND_v04_JWSS_classification'$SLURM_ARRAY_TASK_ID
-TFRECORDS_REGEX='cf100_species002_spont070/*.tfrecords'
+OUTDIR='/saved_models/arch_0628/PND_v04_JWSS_ANlowpass0050Hz_classification'$SLURM_ARRAY_TASK_ID
+TFRECORDS_REGEX='cf100_species002_spont070_lowpass0050Hz/*.tfrecords'
 
 echo "evaluating model in output directory: $OUTDIR"
 echo "evaluation data: $TFRECORDS_REGEX"
@@ -47,19 +48,19 @@ python pitchnet_evaluate_best.py \
 -o "$OUTDIR"
 
 
-singularity exec --nv \
--B /home \
--B /om \
--B /om2 \
--B /om2/user/msaddler/python-packages:/python-packages \
--B $SCRATCH_PATH/data_pitchnet:/data \
--B /om2/user/msaddler/pitchnet/saved_models:/saved_models \
--B /om2/user/msaddler/pitchnet/ibmHearingAid:/code_location \
-/om2/user/msaddler/singularity-images/tfv1.13_unet.simg \
-python pitchnet_evaluate_best.py \
--de "/om/user/msaddler/data_pitchnet/moore1985/Moore1985_MistunedHarmonics_v00/$TFRECORDS_REGEX" \
--efn 'EVAL_MistunedHarm_v00_bestckpt.json' \
--o "$OUTDIR"
+# singularity exec --nv \
+# -B /home \
+# -B /om \
+# -B /om2 \
+# -B /om2/user/msaddler/python-packages:/python-packages \
+# -B $SCRATCH_PATH/data_pitchnet:/data \
+# -B /om2/user/msaddler/pitchnet/saved_models:/saved_models \
+# -B /om2/user/msaddler/pitchnet/ibmHearingAid:/code_location \
+# /om2/user/msaddler/singularity-images/tfv1.13_unet.simg \
+# python pitchnet_evaluate_best.py \
+# -de "/om/user/msaddler/data_pitchnet/moore1985/Moore1985_MistunedHarmonics_v00/$TFRECORDS_REGEX" \
+# -efn 'EVAL_MistunedHarm_v00_bestckpt.json' \
+# -o "$OUTDIR"
 
 
 singularity exec --nv \
@@ -77,16 +78,16 @@ python pitchnet_evaluate_best.py \
 -o "$OUTDIR"
 
 
-singularity exec --nv \
--B /home \
--B /om \
--B /om2 \
--B /om2/user/msaddler/python-packages:/python-packages \
--B $SCRATCH_PATH/data_pitchnet:/data \
--B /om2/user/msaddler/pitchnet/saved_models:/saved_models \
--B /om2/user/msaddler/pitchnet/ibmHearingAid:/code_location \
-/om2/user/msaddler/singularity-images/tfv1.13_unet.simg \
-python pitchnet_evaluate_best.py \
--de "/om/user/msaddler/data_pitchnet/shackcarl1994/AltPhase_v01_f0min080_f0max320/$TFRECORDS_REGEX" \
--efn 'EVAL_AltPhase_v01_bestckpt.json' \
--o "$OUTDIR"
+# singularity exec --nv \
+# -B /home \
+# -B /om \
+# -B /om2 \
+# -B /om2/user/msaddler/python-packages:/python-packages \
+# -B $SCRATCH_PATH/data_pitchnet:/data \
+# -B /om2/user/msaddler/pitchnet/saved_models:/saved_models \
+# -B /om2/user/msaddler/pitchnet/ibmHearingAid:/code_location \
+# /om2/user/msaddler/singularity-images/tfv1.13_unet.simg \
+# python pitchnet_evaluate_best.py \
+# -de "/om/user/msaddler/data_pitchnet/shackcarl1994/AltPhase_v01_f0min080_f0max320/$TFRECORDS_REGEX" \
+# -efn 'EVAL_AltPhase_v01_bestckpt.json' \
+# -o "$OUTDIR"
