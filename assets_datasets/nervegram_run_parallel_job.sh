95500a2c7070d55d7294cc263278cf1025c730e4
Mark Saddler <msaddler@mit.edu>
2019-09-26 10:48:41 -0400

    generating ANlowpass0050Hz versions of transposedtones and freqshifted complexes

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -8,8 +8,8 @@
 #SBATCH --time=0-30:00:00
 #SBATCH --time-min=0-24:00:00
 #SBATCH --exclude=node[001-030]
-#SBATCH --array=0-49
-#SBATCH --partition=mcdermott
+#SBATCH --array=0-23
+##SBATCH --partition=mcdermott
 ##SBATCH --partition=use-everything
 ##SBATCH --requeue
 ##SBATCH --dependency=afterok:14702742
@@ -17,11 +17,11 @@
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
 # source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_TLAS_snr_neg10pos03/augment_halflowpassnoise_v02/PND_sr32000_v04*.hdf5'
 # dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_TLAS_snr_neg10pos03/augment_halflowpassnoise_v02/cf100_species002_spont070/bez2018meanrates.hdf5'
-# source_regex='/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz/*.hdf5'
-# dest_filename='/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz/cf100_species002_spont070_lowpass1000Hz/bez2018meanrates.hdf5'
-source_regex='/om/user/msaddler/data_pitchnet/mooremoore2003/MooreMoore2003_frequencyShiftedComplexes_f0_080to480Hz/*.hdf5'
-dest_filename='/om/user/msaddler/data_pitchnet/mooremoore2003/MooreMoore2003_frequencyShiftedComplexes_f0_080to480Hz/cf100_species002_spont070_lowpass1000Hz/bez2018meanrates.hdf5'
-jobs_per_source_file=50
+source_regex='/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz/*.hdf5'
+dest_filename='/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz/cf100_species002_spont070_lowpass0050Hz/bez2018meanrates.hdf5'
+# source_regex='/om/user/msaddler/data_pitchnet/mooremoore2003/MooreMoore2003_frequencyShiftedComplexes_f0_080to480Hz/*.hdf5'
+# dest_filename='/om/user/msaddler/data_pitchnet/mooremoore2003/MooreMoore2003_frequencyShiftedComplexes_f0_080to480Hz/cf100_species002_spont070_lowpass0050Hz/bez2018meanrates.hdf5'
+jobs_per_source_file=24
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 
