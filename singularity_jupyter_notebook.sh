8d42feebb063bf338ee3bd3a4f6a0795086be4b7
Mark Saddler <msaddler@mit.edu>
2019-09-29 21:38:45 -0400

    re-binding /om4 to jupyter notebook job

--- singularity_jupyter_notebook.sh
+++ singularity_jupyter_notebook.sh
@@ -5,6 +5,7 @@ singularity exec --nv \
 -B /om \
 -B /nobackup \
 -B /om2 \
+-B /om4 \
 -B /om2/user/msaddler/python-packages:/python-packages \
 -B /om/user/msaddler/data_pitchnet:/data \
 -B /om2/user/$user/pitchnet/saved_models:/saved_models \
