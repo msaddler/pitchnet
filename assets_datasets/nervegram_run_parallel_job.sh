347f5020c577be3fe551bc31ded5aa1b06478208
Mark Saddler <msaddler@mit.edu>
2019-10-12 21:53:44 -0400

    generating PNDv05 and PNDv06 TLAS nervegrams

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -5,22 +5,22 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4000
 #SBATCH --nodes=1
-#SBATCH --time=0-30:00:00
-#SBATCH --time-min=0-24:00:00
+#SBATCH --time=0-48:00:00
+#SBATCH --time-min=0-48:00:00
 #SBATCH --exclude=node[001-030]
-#SBATCH --array=0-599
+#SBATCH --array=0-199
 ##SBATCH --partition=mcdermott
 #SBATCH --partition=use-everything
 #SBATCH --requeue
 ##SBATCH --dependency=afterok:14702742
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_TLAS_snr_neg10pos03/PND_sr32000*.hdf5'
-dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_TLAS_snr_neg10pos03/cf100_species002_spont070_lowpass1000Hz/bez2018meanrates.hdf5'
+source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v06/noise_TLAS_snr_neg10pos03/PND_sr32000*.hdf5'
+dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v06/noise_TLAS_snr_neg10pos03/cf100_species002_spont070/bez2018meanrates.hdf5'
 # source_regex='/om/user/msaddler/data_pitchnet/mooremoore2003/MooreMoore2003_frequencyShiftedComplexes_f0_080to480Hz/*.hdf5'
 # dest_filename='/om/user/msaddler/data_pitchnet/mooremoore2003/MooreMoore2003_frequencyShiftedComplexes_f0_080to480Hz/cf100_species002_spont070_lowpass0320Hz/bez2018meanrates.hdf5'
 
-jobs_per_source_file=6
+jobs_per_source_file=2
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 
@@ -31,5 +31,4 @@ python -u nervegram_run_parallel.py \
 -s "${source_regex}" \
 -d "${dest_filename}" \
 -j ${job_idx} \
--jps ${jobs_per_source_file} \
--lpf '1000.0'
+-jps ${jobs_per_source_file} # -lpf '0'
--- assets_datasets/tfrecords_from_hdf5_job.sh
+++ assets_datasets/tfrecords_from_hdf5_job.sh
@@ -5,15 +5,16 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2500
 #SBATCH --nodes=1
-#SBATCH --time=0-0:40:00
-#SBATCH --array=0-49
-#SBATCH --partition=mcdermott
-##SBATCH --partition=use-everything
-###SBATCH --dependency=afterok:12702768
+#SBATCH --time=0-1:40:00
+#SBATCH --array=0-599
+##SBATCH --partition=mcdermott
+#SBATCH --partition=use-everything
+##SBATCH --dependency=afterok:14990544
+#SBATCH --exclude=node[031-077]
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-# source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_JWSS_snr_neg10pos03/cf100_species002_spont070_lowpass0320Hz/bez2018meanrates*.hdf5'
-source_regex='/om/user/msaddler/data_pitchnet/mooremoore2003/MooreMoore2003_frequencyShiftedComplexes_f0_080to480Hz/cf100_species002_spont070_lowpass0320Hz/bez2018meanrates*.hdf5'
+source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_TLAS_snr_neg10pos03/cf100_species002_spont070_lowpass0640Hz/bez2018meanrates*.hdf5'
+# source_regex='/om/user/msaddler/data_pitchnet/mooremoore2003/MooreMoore2003_frequencyShiftedComplexes_f0_080to480Hz/cf100_species002_spont070_lowpass0320Hz/bez2018meanrates*.hdf5'
 jobs_per_source_file=1
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
