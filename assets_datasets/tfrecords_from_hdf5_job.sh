43b17ed7fb83be2ab2039b32a65cc5e10305f2ce
Mark Saddler <msaddler@mit.edu>
2019-10-31 21:12:22 -0400

    generating tfrecords for new bez2018model datasets

--- assets_datasets/tfrecords_from_hdf5_job.sh
+++ assets_datasets/tfrecords_from_hdf5_job.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #
 #SBATCH --job-name=tfrecords_from_hdf5
-#SBATCH --out="slurm-%A_%a.out"
+#SBATCH --out="trash/tfrecords_slurm-%A_%a.out"
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2500
 #SBATCH --nodes=1
 #SBATCH --time=0-1:40:00
-#SBATCH --array=0-199
+#SBATCH --array=0-299
 ##SBATCH --partition=mcdermott
 #SBATCH --partition=use-everything
 ##SBATCH --dependency=afterok:14990544
 ##SBATCH --exclude=node[031-077]
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v06/noise_TLAS_snr_neg10pos03/cf100_species002_spont070/bez2018meanrates_*.hdf5'
+source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_TLAS_snr_neg10pos03/cf100_species002_spont070_BW10eN1_IHC5000Hz_IHC7order/bez2018meanrates_*.hdf5'
 # source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/cf100_species002_spont070_lowpass0640Hz/bez2018meanrates_*.hdf5'
 jobs_per_source_file=1
 offset=0
