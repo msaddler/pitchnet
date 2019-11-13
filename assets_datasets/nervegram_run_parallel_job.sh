640667cf31e1143b92ae69024d0f6b6652743472
Mark Saddler <msaddler@mit.edu>
2019-11-12 22:10:06 -0500

    generating bernox stimuli with bez2018model modifications (sr16000)

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -8,36 +8,24 @@
 #SBATCH --time=0-48:00:00
 #SBATCH --time-min=0-46:00:00
 #SBATCH --exclude=node[001-030]
-#SBATCH --array=0-299
+#SBATCH --array=0-9
 ##SBATCH --partition=mcdermott
-#SBATCH --partition=use-everything
+##SBATCH --partition=use-everything
 #SBATCH --requeue
 ##SBATCH --dependency=afterok:14702742
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_TLAS_snr_neg10pos03/PND_sr32000*.hdf5'
-dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_TLAS_snr_neg10pos03/sr16000_cf100_species002_spont070_BW10eN1_IHC4000Hz_IHC7order/bez2018meanrates.hdf5'
-# source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/*.hdf5'
-# dest_filename='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/cf100_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/bez2018meanrates.hdf5'
+# source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_TLAS_snr_neg10pos03/PND_sr32000*.hdf5'
+# dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v04/noise_TLAS_snr_neg10pos03/sr16000_cf100_species002_spont070_BW10eN1_IHC4000Hz_IHC7order/bez2018meanrates.hdf5'
+source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/*.hdf5'
+dest_filename='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/sr16000_cf100_species002_spont070_BW10eN1_IHC7000Hz_IHC7order/bez2018meanrates.hdf5'
 
-jobs_per_source_file=3
+jobs_per_source_file=10
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 
 export HDF5_USE_FILE_LOCKING=FALSE
 source activate mdlab # Activate conda environment with "cython_bez2018" module installed
-
-python -u nervegram_run_parallel.py \
--s "${source_regex}" \
--d "${dest_filename}" \
--j ${job_idx} \
--jps ${jobs_per_source_file} \
--bwsf '1.0' \
--lpf '4000.0' \
--lpfo '7' \
--sks 'stimuli/signal_in_noise' \
--sksr 'sr' \
--mrsr '16000.0'
 
 # python -u nervegram_run_parallel.py \
 # -s "${source_regex}" \
@@ -45,7 +33,20 @@ python -u nervegram_run_parallel.py \
 # -j ${job_idx} \
 # -jps ${jobs_per_source_file} \
 # -bwsf '1.0' \
-# -lpf '50.0' \
+# -lpf '4000.0' \
 # -lpfo '7' \
-# -sks 'tone_in_noise' \
-# -sksr 'config_tone/fs'
+# -sks 'stimuli/signal_in_noise' \
+# -sksr 'sr' \
+# -mrsr '16000.0'
+
+python -u nervegram_run_parallel.py \
+-s "${source_regex}" \
+-d "${dest_filename}" \
+-j ${job_idx} \
+-jps ${jobs_per_source_file} \
+-bwsf '1.0' \
+-lpf '7000.0' \
+-lpfo '7' \
+-sks 'tone_in_noise' \
+-sksr 'config_tone/fs' \
+-mrsr '16000.0'
