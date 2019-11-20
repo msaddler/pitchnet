1350009bd516945827f8fe29215d2e81207d0736
Mark Saddler <msaddler@mit.edu>
2019-11-20 10:32:44 -0500

    PND_v07 sr20000 bernox psychophysics plots

--- assets_psychophysics/psychophysics_experiment_job.sh
+++ assets_psychophysics/psychophysics_experiment_job.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name=psychophysics
 #SBATCH --out="slurm-%A_%a.out"
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=12
 #SBATCH --mem-per-cpu=8000
-#SBATCH --time=0-01:00:00
-#SBATCH --array=0-7
+#SBATCH --time=0-02:00:00
+#SBATCH --array=0-5
 #SBATCH --partition=mcdermott
 ##SBATCH --partition=use-everything
 #SBATCH --exclude=node[001-030]
@@ -16,7 +16,7 @@ offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 echo $(hostname)
 
-OUTDIR_REGEX='/om2/user/msaddler/pitchnet/saved_models/models_sr16000/arch_0628mod/PND_v04_TLAS_AN_BW*_IHC*Hz_classification*'
+OUTDIR_REGEX='/om2/user/msaddler/pitchnet/saved_models/models_sr20000/arch_0628mod/PND_v0*_TLAS_AN_BW*_IHC*Hz_classification*'
 # OUTDIR_REGEX='/om/scratch/Sun/msaddler/pitchnet/saved_models/arch_search_v00/arch*'
 EFN_PREFIX='EVAL_SOFTMAX_*'
 PRIOR_RANGE='0.5'
