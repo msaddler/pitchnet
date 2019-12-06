51ca0b3961f4e794ff49a7a233f9a8ae989eb980
Mark Saddler <msaddler@mit.edu>
2019-12-06 01:11:21 -0500

    generating sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order nervegrams for extended diagnostics

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -19,8 +19,8 @@ echo $(hostname)
 ## Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
 #source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/PND_sr32000*.hdf5'
 #dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC9000Hz_IHC7order/bez2018meanrates.hdf5'
-source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/*.hdf5'
-dest_filename='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
+source_regex='/om/user/msaddler/data_pitchnet/moore1985/Moore1985_MistunedHarmonics_v00/*.hdf5'
+dest_filename='/om/user/msaddler/data_pitchnet/moore1985/Moore1985_MistunedHarmonics_v00/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
 
 jobs_per_source_file=20
 offset=0
@@ -50,7 +50,7 @@ python -u nervegram_run_parallel.py \
 -bwsf '1.0' \
 -lpf '3000.0' \
 -lpfo '7' \
--sks 'tone_in_noise' \
+-sks 'stimuli/signal' \
 -sksr 'config_tone/fs' \
 -mrsr '20000.0' \
 -spont 'L'
