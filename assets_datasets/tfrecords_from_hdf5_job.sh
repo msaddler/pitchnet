c8caad1448f0e615c09cc2274817a7a7ca3e475a
Mark Saddler <msaddler@mit.edu>
2019-12-07 11:04:29 -0500

    generating sr20000 nervegrams with reduced phaselocking

--- assets_datasets/tfrecords_from_hdf5_job.sh
+++ assets_datasets/tfrecords_from_hdf5_job.sh
@@ -13,7 +13,7 @@
 ##SBATCH --exclude=node[031-077]
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_*/sr20000_cf100_species002_spont070_BW10eN1_IHC9000Hz_IHC7order/bez2018meanrates_*.hdf5'
+source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC1000Hz_IHC7order/bez2018meanrates_*.hdf5'
 # source_regex='/om/user/msaddler/data_pitchnet/*/*/sr20000_cf100_species002_spont070_BW10eN1_IHC9000Hz_IHC7order/bez2018meanrates*.hdf5'
 jobs_per_source_file=1
 offset=0
