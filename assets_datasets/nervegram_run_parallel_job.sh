d927af2f2ae78e7c05e97fa7a9554b625c1f6f64
Mark Saddler <msaddler@mit.edu>
2019-12-07 13:41:38 -0500

    generating transposed tones evaluation dataset with 4x more stimuli

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -8,7 +8,7 @@
 #SBATCH --time=0-48:00:00
 #SBATCH --time-min=0-36:00:00
 #SBATCH --exclude=node[001-030]
-#SBATCH --array=0-299
+#SBATCH --array=0-99
 ##SBATCH --partition=mcdermott
 #SBATCH --partition=use-everything
 #SBATCH --requeue
@@ -17,30 +17,24 @@
 echo $(hostname)
 
 ## Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/PND_sr32000*.hdf5'
-dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC0250Hz_IHC7order/bez2018meanrates.hdf5'
-# source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/*.hdf5'
-# dest_filename='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/sr20000_cf100_species002_spont070_BW10eN1_IHC6000Hz_IHC7order/bez2018meanrates.hdf5'
 
-jobs_per_source_file=3
+# source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/PND_sr32000*.hdf5'
+# dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC0250Hz_IHC7order/bez2018meanrates.hdf5'
+# jobs_per_source_file=3
+
+# source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/*.hdf5'
+# dest_filename='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/sr20000_cf100_species002_spont070_BW10eN1_IHC0250Hz_IHC7order/bez2018meanrates.hdf5'
+# jobs_per_source_file=20
+
+source_regex='/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz_dense4x/*.hdf5'
+dest_filename='/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz_dense4x/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
+jobs_per_source_file=100
+
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 
 export HDF5_USE_FILE_LOCKING=FALSE
 source activate mdlab # Activate conda environment with "cython_bez2018" module installed
-
-python -u nervegram_run_parallel.py \
--s "${source_regex}" \
--d "${dest_filename}" \
--j ${job_idx} \
--jps ${jobs_per_source_file} \
--bwsf '1.0' \
--lpf '250.0' \
--lpfo '7' \
--sks 'stimuli/signal_in_noise' \
--sksr 'sr' \
--mrsr '20000.0' \
--spont 'H'
 
 # python -u nervegram_run_parallel.py \
 # -s "${source_regex}" \
@@ -48,9 +42,35 @@ python -u nervegram_run_parallel.py \
 # -j ${job_idx} \
 # -jps ${jobs_per_source_file} \
 # -bwsf '1.0' \
-# -lpf '6000.0' \
+# -lpf '250.0' \
+# -lpfo '7' \
+# -sks 'stimuli/signal_in_noise' \
+# -sksr 'sr' \
+# -mrsr '20000.0' \
+# -spont 'H'
+
+# python -u nervegram_run_parallel.py \
+# -s "${source_regex}" \
+# -d "${dest_filename}" \
+# -j ${job_idx} \
+# -jps ${jobs_per_source_file} \
+# -bwsf '1.0' \
+# -lpf '250.0' \
 # -lpfo '7' \
 # -sks 'tone_in_noise' \
 # -sksr 'config_tone/fs' \
 # -mrsr '20000.0' \
 # -spont 'H'
+
+python -u nervegram_run_parallel.py \
+-s "${source_regex}" \
+-d "${dest_filename}" \
+-j ${job_idx} \
+-jps ${jobs_per_source_file} \
+-bwsf '1.0' \
+-lpf '3000.0' \
+-lpfo '7' \
+-sks 'stimuli/signal' \
+-sksr 'config_tone/fs' \
+-mrsr '20000.0' \
+-spont 'H'
