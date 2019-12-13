0cb1019d4eb0be567e2044e1a05202d9f9b82129
Mark Saddler <msaddler@mit.edu>
2019-12-13 16:10:54 -0500

    random_filtered_complex_tone_dataset function

--- assets_datasets/stimuli_generate_synthetic_tones.py
+++ assets_datasets/stimuli_generate_synthetic_tones.py
@@ -35,6 +35,97 @@ def random_filtered_complex_tone(f0, fs, dur,
     return signal, signal_filter_params
 
 
+def random_filtered_complex_tone_dataset(hdf5_filename, N,
+                                         fs=32e3,
+                                         dur=0.150,
+                                         amplitude_jitter=0.5,
+                                         phase_modes=['sine'],
+                                         augmentation_filter_params={},
+                                         kwargs_modified_uniform_masking_noise={},
+                                         range_f0=[80.0, 1000.0],
+                                         range_snr=[-10., 10.],
+                                         range_dbspl=[30., 90.],
+                                         out_combined_key='stimuli/signal_in_noise',
+                                         out_signal_key=None,
+                                         out_noise_key=None,
+                                         out_snr_key='snr',
+                                         out_augmentation_prefix='augmentation/',
+                                         random_seed=858,
+                                         disp_step=1000):
+    '''
+    '''
+    # Set random seed
+    np.random.seed(random_seed)
+    # Randomly sample f0s and phase modes
+    list_f0 = np.exp(np.random.uniform(low=np.log(range_f0[0]),
+                                       high=np.log(range_f0[1]),
+                                       size=[N]))
+    list_phase_mode = np.random.choice(phase_modes, size=[N])
+    list_snr = np.random.uniform(low=range_snr[0],
+                                 high=range_snr[1],
+                                 size=[N])
+    list_dbspl = np.random.uniform(low=range_dbspl[0],
+                                   high=range_dbspl[1],
+                                   size=[N])
+    # Define encoding / decoding dictionaries for phase_mode
+    phase_mode_encoding = {'sine':0, 'rand':1, 'sch':2, 'cos':3, 'alt':4}
+    phase_mode_decoding = {0:'sine', 1:'rand', 2:'sch', 3:'cos', 4:'alt'}
+    # Prepare data_dict and config_key_pair_list for hdf5 filewriting
+    data_dict = {
+        'sr': fs,
+        'config_tone/fs': fs,
+        'config_tone/dur': dur,
+        'config_tone/f0_min': range_f0[0],
+        'config_tone/f0_max': range_f0[1],
+        'config_tone/amplitude_jitter': amplitude_jitter,
+    }
+    config_key_pair_list = [(k, k) for k in data_dict.keys()]
+    data_key_pair_list = [] # Will be populated right before initializing hdf5 file
+    # Main loop to generate dataset
+    for itrN in range(0, N):
+        # Generate signal and noise with desired properties
+        f0 = list_f0[itrN]
+        phase_mode = list_phase_mode[itrN]
+        snr = list_snr[itrN]
+        dbspl = list_dbspl[itrN]
+        signal, signal_filter_params = random_filtered_complex_tone(f0, 
+                                                                    fs,
+                                                                    dur,
+                                                                    phase_mode=phase_mode,
+                                                                    amplitude_jitter=amplitude_jitter,
+                                                                    augmentation_filter_params=augmentation_filter_params)
+        noise = stimuli_util.modified_uniform_masking_noise(fs, dur, **kwargs_modified_uniform_masking_noise)
+        signal_and_noise = stimuli_util.combine_signal_and_noise(signal=signal,
+                                                                 noise=noise,
+                                                                 snr=snr,
+                                                                 rms_out=20e-6*np.power(10, dbspl/20))
+        # Prepare data_dict
+        data_dict[out_combined_key] = signal_and_noise.astype(np.float32)
+        data_dict[out_combined_key + 'dBSPL'] = dbspl
+        data_dict[out_snr_key] = snr
+        if out_signal_key is not None: data_dict[out_signal_key] = signal.astype(np.float32)
+        if out_noise_key is not None: data_dict[out_noise_key] = noise.astype(np.float32)
+        for key in signal_filter_params.keys():
+            data_dict[out_augmentation_prefix + 'signal_' + key] = signal_filter_params[key]
+        # Initialize the hdf5 file on the first iteration
+        if itrN == 0:
+            print('[INITIALIZING]: {}'.format(hdf5_filename))
+            for k in data_dict.keys():
+                if not (k, k) in config_key_pair_list:
+                    data_key_pair_list.append((k, k))
+            initialize_hdf5_file(hdf5_filename, N, data_dict, file_mode='w',
+                                 data_key_pair_list=data_key_pair_list,
+                                 config_key_pair_list=config_key_pair_list)
+            hdf5_f = h5py.File(hdf5_filename, 'r+')
+        # Write each data_dict to hdf5 file
+        write_example_to_hdf5(hdf5_f, data_dict, itrN, data_key_pair_list=data_key_pair_list)
+        if itrN % disp_step == 0:
+            print('... signal {} of {} (f0={:.2f}, dbspl={:.2f}, snr={:.2f})'.format(itrN, N, f0, dbspl, snr))
+    # Close hdf5 file
+    hdf5_f.close()
+    print('[END]: {}'.format(hdf5_filename))
+
+
 def lowpass_complex_tone(f0, fs, dur, attenuation_start=1000., attenuation_slope=0.,
                          phase_mode='sine', amp_noise=0.):
     '''
