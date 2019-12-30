a5cb129e6a127984e6e60fae5eb8222bc0a81968
Mark Saddler <msaddler@mit.edu>
2019-12-30 02:26:03 -0500

    generating PNDv08 nervegrams with 2x narrower filters

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -19,7 +19,7 @@ echo $(hostname)
 ## Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
 
 source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/PND_sr32000*.hdf5'
-dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
+dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
 jobs_per_source_file=3
 
 # source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_synthetic/noise_UMNm_snr_neg10pos10_phase0_filter_signalHPv00/PND_sr32000*.hdf5'
@@ -45,7 +45,7 @@ python -u nervegram_run_parallel.py \
 -d "${dest_filename}" \
 -j ${job_idx} \
 -jps ${jobs_per_source_file} \
--bwsf '2.0' \
+-bwsf '0.5' \
 -lpf '3000.0' \
 -lpfo '7' \
 -sks 'stimuli/signal_in_noise' \
