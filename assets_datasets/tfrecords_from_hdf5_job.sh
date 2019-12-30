212b30b8129baa21f2d1a13ee300e9629467f395
Mark Saddler <msaddler@mit.edu>
2019-12-29 19:30:58 -0500

    regenerating tfrecords following nvidia binaries error

--- assets_datasets/tfrecords_from_hdf5_job.sh
+++ assets_datasets/tfrecords_from_hdf5_job.sh
@@ -10,10 +10,10 @@
 ##SBATCH --partition=mcdermott
 #SBATCH --partition=use-everything
 ##SBATCH --dependency=afterok:14990544
-##SBATCH --exclude=node[031-077]
+#SBATCH --exclude=node[001-030]
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10_filter_signal*Pv01/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_*.hdf5'
+source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08inst/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_*.hdf5'
 # source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_synthetic/noise_UMNm_snr_neg10pos10_phase0_filter_signal*Pv00/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
 # source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/sr20000_cf100_species002_spont070_BW10eN1_IHC0250Hz_IHC7order/bez2018meanrates*.hdf5'
 
