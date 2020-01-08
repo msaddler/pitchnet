9371056b0c978e67cb22ddfe1e54d6bbe461a21e
Mark Saddler <msaddler@mit.edu>
2020-01-07 22:11:55 -0500

    generating PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW30eN1_IHC3000Hz_IHC7order tfrecords

--- assets_datasets/tfrecords_from_hdf5_job.sh
+++ assets_datasets/tfrecords_from_hdf5_job.sh
@@ -6,16 +6,16 @@
 #SBATCH --mem-per-cpu=2500
 #SBATCH --nodes=1
 #SBATCH --time=0-1:40:00
-#SBATCH --array=0-19
+#SBATCH --array=0-299
 ##SBATCH --partition=mcdermott
 #SBATCH --partition=use-everything
 ##SBATCH --dependency=afterok:14990544
 #SBATCH --exclude=node[001-030]
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
-# source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
+source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW30eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
 # source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_synthetic/noise_UMNm_snr_neg10pos10_phase0_filter_signal*Pv00/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
-source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/sr20000_cf100_species002_spont070_BW30eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
+# source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/sr20000_cf100_species002_spont070_BW30eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
 
 jobs_per_source_file=1
 offset=0
