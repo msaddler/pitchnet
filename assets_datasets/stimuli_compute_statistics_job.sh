99c13edb7d90702429793b4b398ff8a2f5093b05
Mark Saddler <msaddler@mit.edu>
2020-01-09 01:58:54 -0500

    slurm job script for computing mean power spectra of hdf5 datasets

---
+++ assets_datasets/stimuli_compute_statistics_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --job-name=mean_spectrum
+#SBATCH --out="slurm-%A_%a.out"
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=2000
+#SBATCH --nodes=1
+#SBATCH --time=0-8:00:00
+#SBATCH --time-min=0-8:00:00
+#SBATCH --exclude=node[001-030]
+#SBATCH --partition=mcdermott
+
+source_fn_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/*.hdf5'
+
+export HDF5_USE_FILE_LOCKING=FALSE
+source activate mdlab
+
+python -u stimuli_compute_statistics.py \
+-r "${source_fn_regex}" \
+-k '/stimuli/signal'
