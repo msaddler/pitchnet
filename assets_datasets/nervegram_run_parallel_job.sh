176533b7668a4335e2cc78810ad45bac9fcd2dab
Mark Saddler <msaddler@mit.edu>
2020-04-10 16:04:11 -0400

    generating Moore1985_MistunedHarmonics_v01/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order bez2018model nervegrams

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -5,10 +5,10 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4000
 #SBATCH --nodes=1
-#SBATCH --time=0-48:00:00
-#SBATCH --time-min=0-36:00:00
-#SBATCH --exclude=node[001-030,073]
-#SBATCH --array=0-99
+#SBATCH --time=0-24:00:00
+##SBATCH --time-min=0-24:00:00
+#SBATCH --exclude=node[001-030,073,064]
+#SBATCH --array=0-19
 ##SBATCH --partition=mcdermott
 ##SBATCH --partition=use-everything
 #SBATCH --requeue
@@ -22,17 +22,17 @@ echo $(hostname)
 # dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10_filter_signalBPv02/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
 # jobs_per_source_file=3
 
-source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_synthetic/noise_UMNm_snr_neg10pos10_phase01_filter_signalBPv00/PND_sr32000*.hdf5'
-dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_synthetic/noise_UMNm_snr_neg10pos10_phase01_filter_signalBPv00/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
-jobs_per_source_file=1
+# source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_synthetic/noise_UMNm_snr_neg10pos10_phase01_filter_signalBPv00/PND_sr32000*.hdf5'
+# dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_synthetic/noise_UMNm_snr_neg10pos10_phase01_filter_signalBPv00/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
+# jobs_per_source_file=1
 
 # source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/*.hdf5'
 # dest_filename='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/sr20000_cf100_species002_spont070_BW30eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
 # jobs_per_source_file=20
 
-# source_regex='/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz_dense4x/*.hdf5'
-# dest_filename='/om/user/msaddler/data_pitchnet/oxenham2004/Oxenham2004_transposedTones_f0_080to320Hz_dense4x/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
-# jobs_per_source_file=100
+source_regex='/om/user/msaddler/data_pitchnet/moore1985/Moore1985_MistunedHarmonics_v01/*.hdf5'
+dest_filename='/om/user/msaddler/data_pitchnet/moore1985/Moore1985_MistunedHarmonics_v01/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
+jobs_per_source_file=20
 
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
@@ -40,18 +40,18 @@ job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 export HDF5_USE_FILE_LOCKING=FALSE
 source activate mdlab # Activate conda environment with "cython_bez2018" module installed
 
-python -u nervegram_run_parallel.py \
--s "${source_regex}" \
--d "${dest_filename}" \
--j ${job_idx} \
--jps ${jobs_per_source_file} \
--bwsf '1.0' \
--lpf '3000.0' \
--lpfo '7' \
--sks 'stimuli/signal_in_noise' \
--sksr 'sr' \
--mrsr '20000.0' \
--spont 'H'
+# python -u nervegram_run_parallel.py \
+# -s "${source_regex}" \
+# -d "${dest_filename}" \
+# -j ${job_idx} \
+# -jps ${jobs_per_source_file} \
+# -bwsf '1.0' \
+# -lpf '3000.0' \
+# -lpfo '7' \
+# -sks 'stimuli/signal_in_noise' \
+# -sksr 'sr' \
+# -mrsr '20000.0' \
+# -spont 'H'
 
 # python -u nervegram_run_parallel.py \
 # -s "${source_regex}" \
@@ -66,15 +66,15 @@ python -u nervegram_run_parallel.py \
 # -mrsr '20000.0' \
 # -spont 'H'
 
-# python -u nervegram_run_parallel.py \
-# -s "${source_regex}" \
-# -d "${dest_filename}" \
-# -j ${job_idx} \
-# -jps ${jobs_per_source_file} \
-# -bwsf '0.5' \
-# -lpf '3000.0' \
-# -lpfo '7' \
-# -sks 'stimuli/signal' \
-# -sksr 'config_tone/fs' \
-# -mrsr '20000.0' \
-# -spont 'H'
+python -u nervegram_run_parallel.py \
+-s "${source_regex}" \
+-d "${dest_filename}" \
+-j ${job_idx} \
+-jps ${jobs_per_source_file} \
+-bwsf '1.0' \
+-lpf '3000.0' \
+-lpfo '7' \
+-sks 'stimuli/signal' \
+-sksr 'config_tone/fs' \
+-mrsr '20000.0' \
+-spont 'H'
