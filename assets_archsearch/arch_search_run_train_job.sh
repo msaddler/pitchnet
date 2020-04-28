4cb5b9ba94232ae7759b37a77e44e1a9efee8878
Mark Saddler <msaddler@mit.edu>
2020-04-27 22:43:26 -0400

    preparing to requeue network training jobs that time-out (10 of top 40 archs training on coch manipulations)

--- assets_archsearch/arch_search_run_train_job.sh
+++ assets_archsearch/arch_search_run_train_job.sh
@@ -4,8 +4,8 @@
 #SBATCH --out="slurm-%A_%a.out"
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=18000
-##SBATCH --gres=gpu:QUADRORTX6000:1
-#SBATCH --gres=gpu:GEFORCEGTX1080TI:1
+#SBATCH --gres=gpu:QUADRORTX6000:1
+##SBATCH --gres=gpu:GEFORCEGTX1080TI:1
 #SBATCH --time=0-48:00:00
 ##SBATCH --time-min=0-24:00:00
 #SBATCH --array=87,97,208,245,270,285,287,302,325,373
@@ -17,8 +17,8 @@
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 SOURCE_CONFIG_FN='config_arch_search_v01.json'
-OUTPUT_DIR_PATTERN="/saved_models/arch_search_v01_spont070_BW10eN1_IHC9000Hz_IHC7order/arch_{:04d}"
-OUTPUT_LOG_FN=$(printf "$SCRATCH_PATH/pitchnet/saved_models/arch_search_v01_spont070_BW10eN1_IHC9000Hz_IHC7order/logs_train/arch_%04d.log" ${job_idx})
+OUTPUT_DIR_PATTERN="/saved_models/arch_search_v01_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_{:04d}"
+OUTPUT_LOG_FN=$(printf "$SCRATCH_PATH/pitchnet/saved_models/arch_search_v01_spont070_BW10eN1_IHC3000Hz_IHC7order/logs_train/arch_%04d.log" ${job_idx})
 
 echo $OUTPUT_LOG_FN
 echo $(hostname)
@@ -38,6 +38,6 @@ python /om2/user/msaddler/pitchnet/assets_archsearch/arch_search_run_train.py \
 -o "${OUTPUT_DIR_PATTERN}" \
 -c "${SOURCE_CONFIG_FN}" \
 -j ${job_idx} \
--dt /data/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC9000Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords \
--de /data/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC9000Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords \
+-dt /data/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords \
+-de /data/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords \
 2>&1 | tee $OUTPUT_LOG_FN
