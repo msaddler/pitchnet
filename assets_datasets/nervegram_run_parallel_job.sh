c537ad52ae9daab211664f28ee313c79461f152a
Mark Saddler <msaddler@mit.edu>
2020-05-10 17:43:06 -0400

    generating mcpherson2020/testSPL_v01_f0min080_f0max320_dBSPLmin000_dBSPLmax120/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order nervegrams

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -24,13 +24,13 @@
 # dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_synthetic/noise_UMNm_snr_neg10pos10_phase01_filter_signalBPv00/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
 # jobs_per_source_file=1
 
-source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/*.hdf5'
-dest_filename='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/bez2018meanrates.hdf5'
-jobs_per_source_file=60
+# source_regex='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/*.hdf5'
+# dest_filename='/om/user/msaddler/data_pitchnet/bernox2005/FixedFilter_f0min100_f0max300/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/bez2018meanrates.hdf5'
+# jobs_per_source_file=60
 
-# source_regex='/om/user/msaddler/data_pitchnet/mcpherson2020/testSPL_v01_f0min080_f0max320_dBSPLmin000_dBSPLmax120/*.hdf5'
-# dest_filename='/om/user/msaddler/data_pitchnet/mcpherson2020/testSPL_v01_f0min080_f0max320_dBSPLmin000_dBSPLmax120/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
-# jobs_per_source_file=20
+source_regex='/om/user/msaddler/data_pitchnet/mcpherson2020/testSPL_v01_f0min080_f0max320_dBSPLmin000_dBSPLmax120/*.hdf5'
+dest_filename='/om/user/msaddler/data_pitchnet/mcpherson2020/testSPL_v01_f0min080_f0max320_dBSPLmin000_dBSPLmax120/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/bez2018meanrates.hdf5'
+jobs_per_source_file=60
 
 # source_regex='/om/user/msaddler/data_pitchnet/bernox2006/nharm10_f0min100_f0max300_TENlevel10dB_harmlevel20dBSPL/*.hdf5'
 # dest_filename='/om/user/msaddler/data_pitchnet/bernox2006/nharm10_f0min100_f0max300_TENlevel10dB_harmlevel20dBSPL/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
