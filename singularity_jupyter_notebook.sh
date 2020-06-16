fab489dc429e20594015590c611a22e9e47e6390
Mark Saddler <msaddler@mit.edu>
2020-06-16 17:03:28 -0400

    minor tweak to singularity jupyter-notebook script to support /om2-->/om5 symlink; darkening low snr f0dl schematic spectrogram

--- singularity_jupyter_notebook.sh
+++ singularity_jupyter_notebook.sh
@@ -1,14 +1,12 @@
-user=$(whoami)
-
 singularity exec --nv \
 -B /home \
 -B /om \
 -B /nobackup \
--B /om2 \
+-B /om2/user/msaddler/ \
 -B /om4 \
 -B /om2/user/msaddler/python-packages:/python-packages \
 -B /om/user/msaddler/data_pitchnet:/data \
--B /om2/user/$user/pitchnet/saved_models:/saved_models \
--B /om2/user/$user/pitchnet/ibmHearingAid:/code_location \
+-B /om2/user/msaddler/pitchnet/saved_models:/saved_models \
+-B /om2/user/msaddler/pitchnet/ibmHearingAid:/code_location \
 /om2/user/msaddler/singularity-images/tfv1.13_unet.simg \
 /om2/user/msaddler/jupyter_notebook_job.sh
