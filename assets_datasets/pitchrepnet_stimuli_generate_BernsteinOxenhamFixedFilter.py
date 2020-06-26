1cb5d972edd26b537946b73b044a54cb89828f0b
Mark Saddler <msaddler@mit.edu>
2020-06-25 23:45:39 -0400

    first working copy of pitchrepnet_stimuli_generate_BernsteinOxenhamFixedFilter.py/generate_fixed_bandpass_filter_dataset

--- assets_datasets/pitchrepnet_stimuli_generate_BernsteinOxenhamFixedFilter.py
+++ assets_datasets/pitchrepnet_stimuli_generate_BernsteinOxenhamFixedFilter.py
@@ -49,52 +49,12 @@ def get_bandpass_filter_frequency_response(fl, fh, fs=20e3, order=4):
     return frequency_response_in_dB
 
 
-def shift_bandpass_filter_frequency_response(desired_fl,
-                                             desired_fl_gain_in_dB,
-                                             fs=20e3,
-                                             unshifted_passband=None,
-                                             frequency_response_in_dB=None):
-    '''
-    Accepts a frequency response function and translates it along the frequency axis
-    to a specified low frequency cutoff.
-    
-    Args
-    ----
-    desired_fl (float): low frequency cutoff of the shifted filter (Hz)
-    desired_fl_gain_in_dB (float): desired gain of filter at desired_fl (dB) 
-    fs (int): sampling rate (Hz)
-    unshifted_passband (list or None): if passband (low, high) is not specified, it will be computed
-    frequency_response_in_dB (function): frequency response function to be shifted along frequency axis
-    
-    Returns
-    -------
-    shifted_frequency_response_in_dB (function): frequency response function (dB)
-    '''
-    assert frequency_response_in_dB is not None, "`frequency_response_in_dB` must be specified"
-    # Compute the `desired_fl_gain_in_dB` passband of the unshifted filter
-    if unshifted_passband is None:
-        freq_vector = np.arange(0, fs/2)
-        gain_vector = frequency_response_in_dB(freq_vector)
-        passed_freqs = freq_vector[gain_vector > desired_fl_gain_in_dB]
-        unshifted_passband = [np.min(passed_freqs), np.max(passed_freqs)]
-    # Compute offset between unshifted and desired filter
-    fl_offset = desired_fl - unshifted_passband[0]
-    # Return frequency response function of shifted filter
-    def shifted_frequency_response_in_dB(freqs):
-        shifted_freqs = freqs - fl_offset
-        shifted_freqs[shifted_freqs  < 0.] = 0.
-        shifted_freqs[shifted_freqs  > fs/2] = fs/2
-        return frequency_response_in_dB(shifted_freqs)
-    return shifted_frequency_response_in_dB
-
-
 def harmonic_or_inharmonic_complex_tone(f0,
                                         fs=20e3,
                                         dur=2e0,
                                         inharmonic_jitter_pattern=None,
                                         frequency_response_in_dB=None,
-                                        threshold_dBSPL=33.3,
-                                        component_dBSL=15.0,
+                                        prefilter_component_dBSPL=48.3,
                                         **kwargs_complex_tone):
     '''
     Generates a complex tone (either harmonic or inharmonic) with component
@@ -122,22 +82,25 @@ def harmonic_or_inharmonic_complex_tone(f0,
     # Define component frequencies
     harmonic_freqs = np.arange(f0, fs/2, f0)
     nominal_harmonic_numbers = harmonic_freqs / f0
+    N_component_freqs = nominal_harmonic_numbers.shape[0]
     if inharmonic_jitter_pattern is None:
-        # Component frequencies are harmonic frequencies
-        component_freqs = harmonic_freqs
+        inharmonic_jitter_pattern = np.zeros_like(nominal_harmonic_numbers)
     else:
-        # Component frequencies are harmonic frequencies * inharmonic_jitter_pattern
-        N_component_freqs = nominal_harmonic_numbers.shape[0]
         msg = "inharmonic_jitter_pattern must be at least as long as nominal_harmonic_numbers"
         assert len(inharmonic_jitter_pattern.shape) == 1, msg
         assert inharmonic_jitter_pattern.shape[0] >= N_component_freqs, msg
-        component_freqs = harmonic_freqs * inharmonic_jitter_pattern[:N_component_freqs]
-        component_freqs[component_freqs > fs/2] = fs/2
+        inharmonic_jitter_pattern = inharmonic_jitter_pattern[:N_component_freqs]
+    shift_freqs = f0 * inharmonic_jitter_pattern
+    component_freqs = harmonic_freqs + shift_freqs
+    # Restrict component frequencies and nominal harmonic numbers by Nyquist frequency
+    IDX_BELOW_NYQUIST = component_freqs < fs/2
+    component_freqs = component_freqs[IDX_BELOW_NYQUIST]
+    nominal_harmonic_numbers = nominal_harmonic_numbers[IDX_BELOW_NYQUIST]
     # Define component levels
     if frequency_response_in_dB is None:
         # Return equal-amplitude harmonics if frequency_response_in_dB is not specified
         frequency_response_in_dB = lambda f: np.zeros_like(f)
-    component_dBSPL = threshold_dBSPL + component_dBSL + frequency_response_in_dB(component_freqs)
+    component_dBSPL = prefilter_component_dBSPL + frequency_response_in_dB(component_freqs)
     component_amplitudes = 20e-6 * np.power(10, (component_dBSPL/20))
     # Return signal waveform and dictionary of metadata
     signal = util_stimuli.complex_tone(f0,
@@ -152,9 +115,10 @@ def harmonic_or_inharmonic_complex_tone(f0,
         'fs': fs,
         'dur': dur,
         'nominal_harmonic_numbers': nominal_harmonic_numbers,
-        'jitter_pattern': component_freqs / harmonic_freqs,
+        'inharmonic_jitter_pattern': inharmonic_jitter_pattern,
         'component_freqs': component_freqs,
         'component_dBSPL': component_dBSPL,
+        'prefilter_component_dBSPL': prefilter_component_dBSPL,
     }
     signal_metadata.update(kwargs_complex_tone)
     return signal, signal_metadata
@@ -191,6 +155,7 @@ def generate_fixed_bandpass_filter_dataset(hdf5_filename,
     # Define lists of unique phase modes and jitter modes
     list_unique_phase = np.array([phase_mode_encoding[p] for p in phase_modes])
     list_unique_jitter = np.array(jitter_modes)
+    max_num_harm = int(np.ceil(fs / f0_min))
     # Define list of unique bandpass filter low frequency cutoffs
     if bandpass_fl_spacing.lower() == 'linear':
         list_unique_bandpass_fl = np.linspace(bandpass_fl_min,
@@ -212,28 +177,24 @@ def generate_fixed_bandpass_filter_dataset(hdf5_filename,
     data_dict = {
         'config_tone/fs': fs,
         'config_tone/dur': dur,
-        'config_tone/f0_min': f0_min,
-        'config_tone/f0_max': f0_max,
-        'config_tone/f0_step': f0_step,
         'config_tone/bandpass_fl_min': bandpass_fl_min,
         'config_tone/bandpass_fl_max': bandpass_fl_max,
         'config_tone/bandpass_fl_num': bandpass_fl_num,
         'config_tone/bandpass_fl_spacing': bandpass_fl_spacing,
         'config_tone/bandpass_filter_bw': bandpass_filter_bw,
         'config_tone/bandpass_filter_order': bandpass_filter_order,
+        'config_tone/f0_min': f0_min,
+        'config_tone/f0_max': f0_max,
+        'config_tone/f0_step': f0_step,
         'config_tone/threshold_dBSPL': threshold_dBSPL,
         'config_tone/component_dBSL': component_dBSL,
         'config_tone/noise_dBHzSPL': noise_dBHzSPL,
         'config_tone/noise_attenuation_start': noise_attenuation_start,
         'config_tone/noise_attenuation_slope': noise_attenuation_slope,
+        'config_tone/random_seed': random_seed,
     }
     config_key_pair_list = [(k, k) for k in data_dict.keys()]
     data_key_pair_list = [] # Will be populated right before initializing hdf5 file
-    
-    print(N, len(list_unique_f0), list_unique_f0.min(), list_unique_f0.max(), len(list_unique_bandpass_fl))
-    for k in data_dict.keys():
-        print(k, data_dict[k])
-    return
     
     # Main loop to generate the bandpass filtered tones
     itrN = 0
@@ -242,22 +203,22 @@ def generate_fixed_bandpass_filter_dataset(hdf5_filename,
         frequency_response_in_dB = get_bandpass_filter_frequency_response(fl,
                                                                           fh,
                                                                           fs=fs,
-                                                                          order=bandpass_filter_bw_order)
+                                                                          order=bandpass_filter_order)
         for phase_mode_int in list_unique_phase:
-            for jitter in list_unique_jitter:
-                if jitter is None:
+            # Iterate over jitter_mode and define `get_jitter_pattern` function
+            for jitter_mode in list_unique_jitter:
+                if jitter_mode is None:
                     get_jitter_pattern = lambda: None
+                elif jitter_mode.lower() == 'fixed':
+                    jitter_pattern = np.random.uniform(low=-0.5,
+                                                       high=0.5,
+                                                       size=[max_num_harm])
+                    jitter_pattern[0] = 0
+                    get_jitter_pattern = lambda: jitter_pattern
                 else:
-                    (jitter_type, jitter_max) = jitter
-                    max_harmonics = int(np.ceil(fs / f0_min))
-                    if jitter_type == 'fixed':
-                        jitter_pattern = np.random.uniform(low=-jitter_max,
-                                                           high=jitter_max,
-                                                           size=[max_harmonics])
-                        get_jitter_pattern = lambda: jitter_pattern + 1
-                    else:
-                        raise ValueError("Unrecognized jitter_type = `{}`".format(jitter_type))
-                        
+                    msg = "Unrecognized jitter_mode: `{}` (not yet implemented)"
+                    raise ValueError(msg.format(jitter_mode))
+                # Iterate over f0 values and generate stimuli
                 for f0 in list_unique_f0:
                     # Construct signal
                     signal, signal_metadata = harmonic_or_inharmonic_complex_tone(
@@ -266,8 +227,7 @@ def generate_fixed_bandpass_filter_dataset(hdf5_filename,
                         dur=dur,
                         inharmonic_jitter_pattern=get_jitter_pattern(),
                         frequency_response_in_dB=frequency_response_in_dB,
-                        threshold_dBSPL=threshold_dBSPL,
-                        component_dBSL=component_dBSL,
+                        prefilter_component_dBSPL=threshold_dBSPL+component_dBSL,
                         phase_mode=phase_mode_decoding[phase_mode_int])
                     # Construct noise
                     noise = util_stimuli.modified_uniform_masking_noise(
@@ -276,11 +236,25 @@ def generate_fixed_bandpass_filter_dataset(hdf5_filename,
                         dBHzSPL=noise_dBHzSPL,
                         attenuation_start=noise_attenuation_start,
                         attenuation_slope=noise_attenuation_slope)
-                    
+                    # Combine signal and noise
                     tone_in_noise = signal + noise
+                    # Prepare metadata
+                    IDX_AUDIBLE = signal_metadata['component_dBSPL'] >= threshold_dBSPL
+                    audible_harmonic_numbers = signal_metadata['nominal_harmonic_numbers']
+                    jitter_pattern = signal_metadata['inharmonic_jitter_pattern']
+                    jitter_pattern = np.pad(jitter_pattern,
+                                            (0, max_num_harm-jitter_pattern.shape[0]),
+                                            mode='constant')
+                    # Populate data_dict with signal and metadata
                     data_dict['tone_in_noise'] = tone_in_noise.astype(np.float32)
                     data_dict['f0'] = f0
                     data_dict['phase_mode'] = int(phase_mode_int)
+                    data_dict['low_harm'] = int(np.min(audible_harmonic_numbers))
+                    data_dict['min_audible_harm'] = int(np.min(audible_harmonic_numbers))
+                    data_dict['max_audible_harm'] = int(np.max(audible_harmonic_numbers))
+                    data_dict['bandpass_fl'] = fl
+                    data_dict['bandpass_fh'] = fh
+                    data_dict['jitter_pattern'] = jitter_pattern
                     # Initialize the hdf5 file on the first iteration
                     if itrN == 0:
                         print('[INITIALIZING]: {}'.format(hdf5_filename))
