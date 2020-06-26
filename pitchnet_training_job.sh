e5f7ab8cbe6aa3c985f000c1bdf2c8f07fa25203
Mark Saddler <msaddler@mit.edu>
2020-06-25 21:37:23 -0400

    launching intializations 3-9 of training on speech only / instruments only

--- pitchnet_training_job.sh
+++ pitchnet_training_job.sh
@@ -2,10 +2,10 @@
 #
 #SBATCH --job-name=pitchnet_train
 #SBATCH --out="slurm-%A_%a.out"
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=18000
-##SBATCH --gres=gpu:tesla-v100:1
-#SBATCH --gres=gpu:QUADRORTX6000:1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=20000
+#SBATCH --gres=gpu:tesla-v100:1
+##SBATCH --gres=gpu:QUADRORTX6000:1
 ##SBATCH --gres=gpu:GEFORCEGTX1080TI:1
 ##SBATCH --gres=gpu:GEFORCERTX2080TI:1
 #SBATCH --time=0-48:00:00
@@ -16,10 +16,10 @@
 ##SBATCH --partition=use-everything
 #SBATCH --requeue
 
-OUTDIR='/saved_models/models_sr20000/arch_0302/PND_v08_TLAS_snr_neg10pos10_AN_sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order_classification'$SLURM_ARRAY_TASK_ID
+OUTDIR='/saved_models/models_sr20000/arch_0302/PND_v08spch_TLAS_snr_neg10pos10_AN_BW10eN1_IHC3000Hz_classification'$SLURM_ARRAY_TASK_ID
 OUTPUT_LOG_FN='/om2/user/msaddler/pitchnet'$OUTDIR'/output_train.log'
-DATA_TRAIN='/data/PND_v08/noise_TLAS_snr_neg10pos10/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords'
-DATA_EVAL='/data/PND_v08/noise_TLAS_snr_neg10pos10/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords'
+DATA_TRAIN='/data/PND_v08spch/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords'
+DATA_EVAL='/data/PND_v08spch/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords'
 
 echo $OUTDIR
 echo $OUTPUT_LOG_FN
