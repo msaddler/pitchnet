c84de998ebe548f0011434dc84919c214752ffd9
Mark Saddler <msaddler@mit.edu>
2020-07-23 17:43:02 -0400

    computing mean power spectra, spectral envelopes, and mfccs of filtered natural sounds datasets

--- assets_datasets/stimuli_compute_statistics_job.sh
+++ assets_datasets/stimuli_compute_statistics_job.sh
@@ -11,7 +11,7 @@
 ##SBATCH --partition=mcdermott
 #SBATCH --array=0-99
 
-source_fn_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/*.hdf5'
+source_fn_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10_filter_signalHPv00/*.hdf5'
 # source_fn_regex="$SCRATCH_PATH"'/data_pitchnet/PND_synthetic/noise_UMNm_snr_neg10pos10_phase01_filter_signalLPv00/*.hdf5'
 
 offset=0
