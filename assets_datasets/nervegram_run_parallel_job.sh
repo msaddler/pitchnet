2293d5ca0539c7715f9230e1fc1ecedee5efa199
Mark Saddler <msaddler@mit.edu>
2020-09-09 21:22:05 -0400

    regenerating reduced IHC filter cutoff PNDv08 training dataset nervegrams

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -3,11 +3,11 @@
 #SBATCH --job-name=bez2018model
 #SBATCH --out="trash/slurm-%A_%a.out"
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=5000
+#SBATCH --mem=2000
 #SBATCH --nodes=1
 #SBATCH --time=24:00:00
 ##SBATCH --time-min=0-24:00:00
-#SBATCH --exclude=node[001-030,074]
+#SBATCH --exclude=node[001-030]
 #SBATCH --array=0-299
 ##SBATCH --partition=mcdermott
 ##SBATCH --partition=use-everything
@@ -27,7 +27,7 @@
 # jobs_per_source_file=3
 
 source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/PND_*.hdf5'
-dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
+dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC1000Hz_IHC7order/bez2018meanrates.hdf5'
 jobs_per_source_file=3
 
 # source_regex='/om/user/msaddler/data_pitchnet/mcpherson2020/testSPL_v01_f0min080_f0max320_dBSPLmin000_dBSPLmax120/*.hdf5'
@@ -78,8 +78,8 @@ python -u nervegram_run_parallel.py \
 -d "${dest_filename}" \
 -j ${job_idx} \
 -jps ${jobs_per_source_file} \
--bwsf '0.5' \
--lpf '3000.0' \
+-bwsf '1.0' \
+-lpf '1000.0' \
 -lpfo '7' \
 -sks 'stimuli/signal_in_noise' \
 -sksr 'sr' \
