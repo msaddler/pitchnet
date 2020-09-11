df65bb0e96a11d178d76c0dcff2b1957b0b8f775
msaddler <msaddler@mit.edu>
2020-09-11 17:07:51 -0400

    Satori architecture search parallelization

---
+++ assets_archsearch/arch_search_run_train_satori_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=pitchnet_train
+#SBATCH --out="slurm-%A_%a.out"
+##SBATCH --error="slurm-%A_%a.err"
+##SBATCH --mail-user=msaddler@mit.edu
+##SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:4
+#SBATCH --gpus-per-node=4
+#SBATCH --nodes=1
+##SBATCH --ntasks-per-node=4
+#SBATCH --mem=0
+#SBATCH --time=24:00:00
+#SBATCH --exclusive
+
+## Ensure `parallel` can be found
+export PATH=$HOME/opt/bin:$PATH
+
+parallel -j4 ./arch_search_run_train_satori.sh {1} {%} {#} ::: $(seq 275 299)
