963eecee4f02c922665a95365b01dcbaa3cc4402
Mark Saddler <msaddler@mit.edu>
2020-09-12 21:14:46 -0400

    generating PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC0250Hz_IHC7order/bez2018meanrates nervegrams

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -27,7 +27,7 @@
 # jobs_per_source_file=3
 
 source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/PND_*.hdf5'
-dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
+dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC0250Hz_IHC7order/bez2018meanrates.hdf5'
 jobs_per_source_file=3
 
 # source_regex='/om/user/msaddler/data_pitchnet/mcpherson2020/testSPL_v01_f0min080_f0max320_dBSPLmin000_dBSPLmax120/*.hdf5'
@@ -78,8 +78,8 @@ python -u nervegram_run_parallel.py \
 -d "${dest_filename}" \
 -j ${job_idx} \
 -jps ${jobs_per_source_file} \
--bwsf '2.0' \
--lpf '3000.0' \
+-bwsf '1.0' \
+-lpf '250.0' \
 -lpfo '7' \
 -sks 'stimuli/signal_in_noise' \
 -sksr 'sr' \
