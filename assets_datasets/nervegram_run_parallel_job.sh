a696e46a40cf6004d70d854e52bcc96da7d68a58
Mark Saddler <msaddler@mit.edu>
2020-09-15 18:26:24 -0400

    generating data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order nervegrams after ANmodel bugfix

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -5,10 +5,10 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=7000
 #SBATCH --nodes=1
-#SBATCH --time=24:00:00
+#SBATCH --time=0-24:00:00
 ##SBATCH --time-min=0-24:00:00
 #SBATCH --exclude=node[001-030]
-#SBATCH --array=0-104
+#SBATCH --array=0-299
 ##SBATCH --partition=mcdermott
 ##SBATCH --partition=use-everything
 #SBATCH --requeue
@@ -26,11 +26,11 @@
 # dest_filename="$SCRATCH_PATH"'/data_pitchnet/PND_mfcc/PNDv08PYSnegated12_TLASmatched12_snr_neg10pos10_phase3/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
 # jobs_per_source_file=3
 
-# source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/PND_*.hdf5'
-# jobs_per_source_file=3
+source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/PND_*.hdf5'
+jobs_per_source_file=3
 
-source_regex='/om/user/msaddler/data_pitchnet/*/*v01*/stim.hdf5'
-jobs_per_source_file=15
+# source_regex='/om/user/msaddler/data_pitchnet/*/*v01*/stim.hdf5'
+# jobs_per_source_file=15
 
 offset=0
 job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
@@ -39,18 +39,18 @@ export HDF5_USE_FILE_LOCKING=FALSE
 source activate mdlab # Activate conda environment with "cython_bez2018" module installed
 echo $(hostname)
 
-dest_filename='sr20000_cf100_species002_spont070_BW10eN1_IHC9000Hz_IHC7order'
+dest_filename='sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order'
 python -u nervegram_run_parallel.py \
 -s "${source_regex}" \
 -d "${dest_filename}" \
 -j ${job_idx} \
 -jps ${jobs_per_source_file} \
 -bwsf '1.0' \
--lpf '9000.0' \
+-lpf '50.0' \
 -lpfo '7' \
 -sks 'auto' \
 -sksr 'sr' \
--mrsr '20000.0' \
+-mrsr '2000.0' \
 -spont '70.0' \
--ncf 100 \
+-ncf 1000 \
 -nst 1
