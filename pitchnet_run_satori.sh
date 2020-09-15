b3320d5741c87523750781d4738037c3d12f44f1
msaddler <msaddler@mit.edu>
2020-09-14 22:54:38 -0400

    updated generic satori pitchnet_run script for training specific lists of networks

--- pitchnet_run_satori.sh
+++ pitchnet_run_satori.sh
@@ -1,29 +1,76 @@
 job_idx=$(( $1 - 0 ))
-CUDA_VISIBLE_DEVICES=$(( $2 - 0 ))
+SINGULARITYENV_CUDA_VISIBLE_DEVICES=$(( $2 - 0 ))
 PARALLEL_JOB_COUNTER=$(( $3 - 0 ))
 PARALLEL_SLOT_NUMBER=$(( $4 - 0 ))
 
 export PATH=$HOME/opt/bin:$PATH
 
-echo "=============================="
-echo "job_idx=$job_idx"
-echo "HOSTNAME::CUDA_VISIBLE_DEVICES=$HOSTNAME::GPU-$CUDA_VISIBLE_DEVICES"
-echo "PARALLEL_JOB_COUNTER=$PARALLEL_JOB_COUNTER"
-echo "PARALLEL_SLOT_NUMBER=$PARALLEL_SLOT_NUMBER"
-echo "=============================="
+echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
+echo "|. job_idx=$job_idx"
+echo ".| (slot::host::gpu)=$PARALLEL_SLOT_NUMBER::$HOSTNAME::GPU-$SINGULARITYENV_CUDA_VISIBLE_DEVICES"
+echo "|. PARALLEL_JOB_COUNTER=$PARALLEL_JOB_COUNTER"
+echo "-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_"
 
-DATA_PATH="/nobackup/users/msaddler/data_pitchnet"
-SAVED_MODELS_PATH="/nobackup/users/msaddler/pitchnet/saved_models"
-CODE_LOCATION_PATH="/nobackup/users/msaddler/pitchnet/ibmHearingAid"
+PATH_DATA="/nobackup/users/msaddler/data_pitchnet"
+PATH_SAVED_MODELS="/nobackup/users/msaddler/pitchnet/saved_models"
+PATH_CODE_LOCATION="/nobackup/users/msaddler/pitchnet/ibmHearingAid"
 
-OUTDIR=$(printf "/saved_models/arch_search_v02/arch_%04d" ${job_idx})
-TFRECORDS_REGEX='sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/*.tfrecords'
-EFN_PREFIX='EVAL_SOFTMAX_'
-WRITE_PROBS_OUT=1
+## CHOOSE THE OUTPUT DIRECTORY FROM A HARD-CODED LIST
+declare -a outdir_list=(
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed0"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed1"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed2"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed3"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed4"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed5"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed6"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed7"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed8"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed9"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed0"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed1"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed2"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed3"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed4"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed5"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed6"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed7"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed8"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302_seed9"
+)
+OUTDIR=${outdir_list[$job_idx]}
 
+## CHOOSE THE DATA_TAG BASED ON THE OUTPUT DIRECTORY
+if [[ "$OUTDIR" == *"sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order"* ]]; then
+  DATA_TAG="sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order"
+fi
+if [[ "$OUTDIR" == *"sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order"* ]]; then
+  DATA_TAG="sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order"
+fi
+
+DATA_TRAIN='/data/PND_v08/noise_TLAS_snr_neg10pos10/'$DATA_TAG'/bez2018meanrates_0[0-7]*.tfrecords'
+DATA_EVAL='/data/PND_v08/noise_TLAS_snr_neg10pos10/'$DATA_TAG'/bez2018meanrates_0[8-9]*.tfrecords'
+OUTPUT_LOG_FN=$OUTDIR'/output_train.log'
+
+export SINGULARITYENV_CUDA_VISIBLE_DEVICES
+singularity exec --nv \
+-B $PATH_DATA:/data \
+-B $PATH_SAVED_MODELS:/saved_models \
+-B $PATH_CODE_LOCATION:/code_location \
+docker://afrancl/ibm-hearing-aid-satori:tensorflow \
+./pitchnet_run_train.sh $OUTDIR $DATA_TRAIN $DATA_EVAL $OUTPUT_LOG_FN
+
+
+
+# OUTDIR=$(printf "/saved_models/arch_search_v02/arch_%04d" ${job_idx})
+# TFRECORDS_REGEX='sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/*.tfrecords'
+# EFN_PREFIX='EVAL_SOFTMAX_'
+# WRITE_PROBS_OUT=1
+
+# export SINGULARITYENV_CUDA_VISIBLE_DEVICES
 # singularity exec --nv \
-# -B $DATA_PATH:/data \
-# -B $SAVED_MODELS_PATH:/saved_models \
-# -B $CODE_LOCATION_PATH:/code_location \
+# -B $PATH_DATA:/data \
+# -B $PATH_SAVED_MODELS:/saved_models \
+# -B $PATH_CODE_LOCATION:/code_location \
 # docker://afrancl/ibm-hearing-aid-satori:tensorflow \
 # ./pitchnet_run_eval.sh $OUTDIR $TFRECORDS_REGEX $EFN_PREFIX $WRITE_PROBS_OUT
--- pitchnet_run_satori_job.sh
+++ pitchnet_run_satori_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=pitchnet_run
-#SBATCH --out="slurm-%A_%a.out"
-##SBATCH --error="slurm-%A_%a.err"
+#SBATCH --job-name=pitchnet_train
+#SBATCH --out="slurm-%j.out"
+##SBATCH --error="slurm-%j.err"
 ##SBATCH --mail-user=msaddler@mit.edu
 ##SBATCH --mail-type=ALL
 #SBATCH --gres=gpu:4
@@ -10,27 +10,20 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --mem=0
 #SBATCH --time=24:00:00
+#SBATCH --qos=sched_level_1
 #SBATCH --exclusive
-
-# SLURM_JOB_NUM_NODES=2
-# SLURM_JOB_NODELIST="service0001,service0002"
-# SLURM_NTASKS_PER_NODE=4
-# SLURM_JOB_ID='IMAGINARYTESTJOB123'
 
 ## Create file containing SLURM node list
 SLURM_TASK_LOG_FILENAME="slurm-tasklist-${SLURM_JOB_ID}.out"
 SLURM_JOB_NODELIST_FILENAME="slurm-nodelist-${SLURM_JOB_ID}.out" 
-# echo $SLURM_JOB_NODELIST | sed s/\,/\\n/g > $SLURM_JOB_NODELIST_FILENAME
 srun -l bash -c 'hostname' |  sort -k 2 -u | awk -vORS=, '{print $2}' | sed 's/,$//' > $SLURM_JOB_NODELIST_FILENAME
 
-echo "==========================================================="
 echo "wd=$(pwd)"
 echo "SLURM_JOB_NODELIST=$SLURM_JOB_NODELIST"
 echo "SLURM_JOB_NUM_NODES=$SLURM_JOB_NUM_NODES"
 echo "SLURM_NTASKS_PER_NODE=$SLURM_NTASKS_PER_NODE"
 echo "SLURM_JOB_NODELIST_FILENAME=$SLURM_JOB_NODELIST_FILENAME"
 echo "SLURM_TASK_LOG_FILENAME=$SLURM_TASK_LOG_FILENAME"
-echo "==========================================================="
 
 ## Ensure `parallel` can be found
 export PATH=$HOME/opt/bin:$PATH
@@ -46,4 +39,4 @@ parallel \
 --slf $SLURM_JOB_NODELIST_FILENAME \
 --joblog $SLURM_TASK_LOG_FILENAME \
 --wd $(pwd) \
-./pitchnet_run_satori.sh $PARALLEL_ARGUMENT_STRING ::: $(seq 0 25)
+./pitchnet_run_satori.sh $PARALLEL_ARGUMENT_STRING ::: $(seq 0 9)
