7bfa4a9857e12d31b8172acaf243f3a095260ab2
msaddler <msaddler@mit.edu>
2020-09-17 16:25:27 -0400

    submitting satori training job for cochlear filter BW manipulation networks with top10 arch_search_v02 architectures

--- pitchnet_run_satori.sh
+++ pitchnet_run_satori.sh
@@ -59,6 +59,46 @@ declare -a outdir_list=(
     "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0302_seed7"
     "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0302_seed8"
     "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0302_seed9"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0083"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0154"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0190"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0191"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0286"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0288"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0302"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0335"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0338"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0346"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0083"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0154"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0190"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0191"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0286"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0288"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0302"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0335"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0338"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont1eN1_BW10eN1_IHC3000Hz_IHC7order/arch_0346"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/arch_0083"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/arch_0154"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/arch_0190"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/arch_0191"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/arch_0286"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/arch_0288"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/arch_0302"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/arch_0335"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/arch_0338"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW05eN1_IHC3000Hz_IHC7order/arch_0346"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0083"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0154"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0190"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0191"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0286"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0288"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0302"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0335"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0338"
+    "/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order/arch_0346"
 )
 OUTDIR=${outdir_list[$job_idx]}
 
@@ -76,32 +116,32 @@ if [[ "$OUTDIR" == *"sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7ord
   DATA_TAG="sr20000_cf100_species002_spont070_BW20eN1_IHC3000Hz_IHC7order"
 fi
 
-# DATA_TRAIN='/data/PND_v08/noise_TLAS_snr_neg10pos10/'$DATA_TAG'/bez2018meanrates_0[0-7]*.tfrecords'
-# DATA_EVAL='/data/PND_v08/noise_TLAS_snr_neg10pos10/'$DATA_TAG'/bez2018meanrates_0[8-9]*.tfrecords'
-# OUTPUT_LOG_FN=$OUTDIR'/output_train.log'
+DATA_TRAIN='/data/PND_v08/noise_TLAS_snr_neg10pos10/'$DATA_TAG'/bez2018meanrates_0[0-7]*.tfrecords'
+DATA_EVAL='/data/PND_v08/noise_TLAS_snr_neg10pos10/'$DATA_TAG'/bez2018meanrates_0[8-9]*.tfrecords'
+OUTPUT_LOG_FN=$OUTDIR'/output_train.log'
 
-# echo "[START TRAINING] $OUTPUT_LOG_FN" &> $(printf "slurm_run_satori-%04d.out" ${job_idx})
-# export SINGULARITYENV_CUDA_VISIBLE_DEVICES
-# singularity exec --nv \
-# -B $PATH_DATA:/data \
-# -B $PATH_SAVED_MODELS:/saved_models \
-# -B $PATH_CODE_LOCATION:/code_location \
-# docker://afrancl/ibm-hearing-aid-satori:tensorflow \
-# ./pitchnet_run_train.sh $OUTDIR $DATA_TRAIN $DATA_EVAL $OUTPUT_LOG_FN
-# echo "[END TRAINING] $OUTPUT_LOG_FN" >> $(printf "slurm_run_satori-%04d.out" ${job_idx})
-
-
-# OUTDIR=$(printf "/saved_models/arch_search_v02/arch_%04d" ${job_idx})
-TFRECORDS_REGEX="$DATA_TAG/*.tfrecords"
-EFN_PREFIX='EVAL_SOFTMAX_'
-OUTPUT_LOG_FN=$OUTDIR'/output_eval.log'
-
-echo "[START EVALUATION] $OUTPUT_LOG_FN" &> $(printf "slurm_run_eval_satori-%04d.out" ${job_idx})
+echo "[START TRAINING] $OUTPUT_LOG_FN" &> $(printf "slurm_run_train_satori-%04d.out" ${job_idx})
 export SINGULARITYENV_CUDA_VISIBLE_DEVICES
 singularity exec --nv \
 -B $PATH_DATA:/data \
 -B $PATH_SAVED_MODELS:/saved_models \
 -B $PATH_CODE_LOCATION:/code_location \
 docker://afrancl/ibm-hearing-aid-satori:tensorflow \
-./pitchnet_run_eval.sh $OUTDIR $TFRECORDS_REGEX $EFN_PREFIX $OUTPUT_LOG_FN
-echo "[END EVALUATION] $OUTPUT_LOG_FN" >> $(printf "slurm_run_eval_satori-%04d.out" ${job_idx})
+./pitchnet_run_train.sh $OUTDIR $DATA_TRAIN $DATA_EVAL $OUTPUT_LOG_FN
+echo "[END TRAINING] $OUTPUT_LOG_FN" >> $(printf "slurm_run_train_satori-%04d.out" ${job_idx})
+
+
+# # OUTDIR=$(printf "/saved_models/arch_search_v02/arch_%04d" ${job_idx})
+# TFRECORDS_REGEX="$DATA_TAG/*.tfrecords"
+# EFN_PREFIX='EVAL_SOFTMAX_'
+# OUTPUT_LOG_FN=$OUTDIR'/output_eval.log'
+
+# echo "[START EVALUATION] $OUTPUT_LOG_FN" &> $(printf "slurm_run_eval_satori-%04d.out" ${job_idx})
+# export SINGULARITYENV_CUDA_VISIBLE_DEVICES
+# singularity exec --nv \
+# -B $PATH_DATA:/data \
+# -B $PATH_SAVED_MODELS:/saved_models \
+# -B $PATH_CODE_LOCATION:/code_location \
+# docker://afrancl/ibm-hearing-aid-satori:tensorflow \
+# ./pitchnet_run_eval.sh $OUTDIR $TFRECORDS_REGEX $EFN_PREFIX $OUTPUT_LOG_FN
+# echo "[END EVALUATION] $OUTPUT_LOG_FN" >> $(printf "slurm_run_eval_satori-%04d.out" ${job_idx})
