869afa07f87d49df484445147b0edc818bf33421
Mark Saddler <msaddler@mit.edu>
2020-09-21 22:22:46 -0400

    generating PNDv08inst and PNDv08spch sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order nervegrams

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -22,7 +22,7 @@
 # dest_filename="$DIR_DEST"'/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates.hdf5'
 # jobs_per_source_file=50
 
-source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_mfcc/PNDv08PYSmatched12_TLASmatched12_snr_neg10pos10_phase3/*.hdf5'
+source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08spch/noise_TLAS_snr_neg10pos10/*.hdf5'
 jobs_per_source_file=3
 
 # source_regex='/om/user/msaddler/data_pitchnet/*/*v01*/stim.hdf5'
