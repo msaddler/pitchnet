be45d5e62069d2b1dfd628d990072f80b6f9f92f
Mark Saddler <msaddler@mit.edu>
2020-09-22 21:13:10 -0400

    submitting training jobs for PND_mfcc statistically matched and anti-matched networks

--- pitchnet_training_job.sh
+++ pitchnet_training_job.sh
@@ -10,20 +10,20 @@
 ##SBATCH --gres=gpu:GEFORCERTX2080TI:1
 #SBATCH --time=0-48:00:00
 #SBATCH --time-min=0-24:00:00
-#SBATCH --array=0-9
-##SBATCH --array=83,154,190,191,286,288,302,335,338,346
+##SBATCH --array=0-9
+#SBATCH --array=83,154,190,191,286,288,302,335,338,346
 ##SBATCH --exclude=node063
 ##SBATCH --partition=mcdermott
 ##SBATCH --partition=use-everything
 ##SBATCH --dependency=afterok:17887282
 #SBATCH --requeue
 
-# ZPJID=$(printf "%04d" $SLURM_ARRAY_TASK_ID)
-# OUTDIR='/saved_models/arch_search_v02_topN/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/arch_'$ZPJID
-OUTDIR='/saved_models/arch_search_v02_topN/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/arch_0191_seed'$SLURM_ARRAY_TASK_ID
+ZPJID=$(printf "%04d" $SLURM_ARRAY_TASK_ID)
+OUTDIR='/saved_models/arch_search_v02_topN/PND_mfcc_PNDv08PYSnegated12_TLASmatched12_snr_neg10pos10_phase3/arch_'$ZPJID
+# OUTDIR='/saved_models/arch_search_v02_topN/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/arch_0191_seed'$SLURM_ARRAY_TASK_ID
 OUTPUT_LOG_FN='/om2/user/msaddler/pitchnet'$OUTDIR'/output_train.log'
-DATA_TRAIN='/data/PND_v08/noise_TLAS_snr_neg10pos10/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords'
-DATA_EVAL='/data/PND_v08/noise_TLAS_snr_neg10pos10/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords'
+DATA_TRAIN='/data/PND_mfcc/PNDv08PYSnegated12_TLASmatched12_snr_neg10pos10_phase3/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords'
+DATA_EVAL='/data/PND_mfcc/PNDv08PYSnegated12_TLASmatched12_snr_neg10pos10_phase3/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords'
 
 echo $OUTDIR
 echo $OUTPUT_LOG_FN
