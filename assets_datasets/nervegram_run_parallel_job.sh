06b8c618f4c8107cf7a9a9e255beeb24b5a834c2
Mark Saddler <msaddler@mit.edu>
2020-09-23 16:34:40 -0400

    generating bernox2005/neurophysiology_SlidingFixedFilter_lharm01to30_phase0_f0min080_f0max320/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order nervegrams

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -3,12 +3,12 @@
 #SBATCH --job-name=bez2018model
 #SBATCH --out="trash/slurm-%A_%a.out"
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=7000
+#SBATCH --mem=4000
 #SBATCH --nodes=1
 #SBATCH --time=0-24:00:00
 ##SBATCH --time-min=0-24:00:00
 #SBATCH --exclude=node[001-030]
-#SBATCH --array=0-104
+#SBATCH --array=0-14
 ##SBATCH --partition=mcdermott
 ##SBATCH --partition=use-everything
 #SBATCH --requeue
@@ -25,7 +25,7 @@
 # source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08spch/noise_TLAS_snr_neg10pos10/*.hdf5'
 # jobs_per_source_file=3
 
-source_regex='/om/user/msaddler/data_pitchnet/*/*v01*/stim.hdf5'
+source_regex='/om/user/msaddler/data_pitchnet/bernox2005/neurophysiology_SlidingFixedFilter_lharm01to30_phase0_f0min080_f0max320/stim.hdf5'
 jobs_per_source_file=15
 
 offset=0
@@ -35,18 +35,18 @@ export HDF5_USE_FILE_LOCKING=FALSE
 source activate mdlab # Activate conda environment with "cython_bez2018" module installed
 echo $(hostname)
 
-dest_filename='sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order'
+dest_filename='sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order'
 python -u nervegram_run_parallel.py \
 -s "${source_regex}" \
 -d "${dest_filename}" \
 -j ${job_idx} \
 -jps ${jobs_per_source_file} \
 -bwsf '1.0' \
--lpf '50.0' \
+-lpf '3000.0' \
 -lpfo '7' \
 -sks 'auto' \
 -sksr 'sr' \
--mrsr '2000.0' \
+-mrsr '20000.0' \
 -spont '70.0' \
--ncf 1000 \
+-ncf 100 \
 -nst 1
