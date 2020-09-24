cdb00d0d16ec109b9ed321b22e9679714fd253ae
Mark Saddler <msaddler@mit.edu>
2020-09-24 19:57:11 -0400

    setting up model directories and launching training jobs for sr20000_cf100_species004_spont070_BWlinear_IHC3000Hz_IHC7order networks (linearized cochlea)

--- pitchnet_training_job.sh
+++ pitchnet_training_job.sh
@@ -19,11 +19,11 @@
 #SBATCH --requeue
 
 ZPJID=$(printf "%04d" $SLURM_ARRAY_TASK_ID)
-OUTDIR='/saved_models/arch_search_v02_topN/PND_v08spch_noise_TLAS_snr_neg10pos10/arch_'$ZPJID
+OUTDIR='/saved_models/arch_search_v02_topN/sr20000_cf100_species004_spont070_BWlinear_IHC3000Hz_IHC7order/arch_'$ZPJID
 # OUTDIR='/saved_models/arch_search_v02_topN/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/arch_0191_seed'$SLURM_ARRAY_TASK_ID
 OUTPUT_LOG_FN='/om2/user/msaddler/pitchnet'$OUTDIR'/output_train.log'
-DATA_TRAIN='/data/PND_v08spch/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords'
-DATA_EVAL='/data/PND_v08spch/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords'
+DATA_TRAIN='/data/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species004_spont070_BWlinear_IHC3000Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords'
+DATA_EVAL='/data/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species004_spont070_BWlinear_IHC3000Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords'
 
 echo $OUTDIR
 echo $OUTPUT_LOG_FN
