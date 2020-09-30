cf61ab9706fd61e32e0ae6dedc936a95896fed44
Mark Saddler <msaddler@mit.edu>
2020-09-29 23:50:01 -0400

    training arch_search_v02_topN/PND_v08_noise_TLAS_snr_pos50pos70 top10arch networks

--- pitchnet_training_job.sh
+++ pitchnet_training_job.sh
@@ -5,10 +5,10 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=18000
 ##SBATCH --gres=gpu:tesla-v100:1
-##SBATCH --gres=gpu:QUADRORTX6000:1
-#SBATCH --gres=gpu:GEFORCEGTX1080TI:1
+#SBATCH --gres=gpu:QUADRORTX6000:1
+##SBATCH --gres=gpu:GEFORCEGTX1080TI:1
 ##SBATCH --gres=gpu:GEFORCERTX2080TI:1
-#SBATCH --time=1-48:00:00
+#SBATCH --time=1-24:00:00
 ##SBATCH --time-min=0-24:00:00
 ##SBATCH --array=0-9
 #SBATCH --array=83,154,190,191,286,288,302,335,338,346
@@ -19,11 +19,11 @@
 #SBATCH --requeue
 
 ZPJID=$(printf "%04d" $SLURM_ARRAY_TASK_ID)
-OUTDIR='/saved_models/arch_search_v02_topN/REDOsr20000_cf100_species002_spont070_BW10eN1_IHC1000Hz_IHC7order/arch_'$ZPJID
+OUTDIR='/saved_models/arch_search_v02_topN/PND_v08_noise_TLAS_snr_pos50pos70/arch_'$ZPJID
 # OUTDIR='/saved_models/arch_search_v02_topN/sr2000_cf1000_species002_spont070_BW10eN1_IHC0050Hz_IHC7order/arch_0191_seed'$SLURM_ARRAY_TASK_ID
 OUTPUT_LOG_FN='/om2/user/msaddler/pitchnet'$OUTDIR'/output_train.log'
-DATA_TRAIN='/data/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC1000Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords'
-DATA_EVAL='/data/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC1000Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords'
+DATA_TRAIN='/data/PND_v08/noise_TLAS_snr_pos50pos70/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords'
+DATA_EVAL='/data/PND_v08/noise_TLAS_snr_pos50pos70/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords'
 
 echo $OUTDIR
 echo $OUTPUT_LOG_FN
