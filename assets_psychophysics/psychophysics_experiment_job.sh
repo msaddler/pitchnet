c0f5997bff86e631ced38d104cfc8cd63be5383e
Mark Saddler <msaddler@mit.edu>
2020-10-09 15:09:50 -0400

    preliminary psychophysical results of SNR manipulation networks and lowharm_v02 and lowharm_v03 stimuli

--- assets_psychophysics/psychophysics_experiment_job.sh
+++ assets_psychophysics/psychophysics_experiment_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=36000
 #SBATCH --time=0-6:00:00
-#SBATCH --array=0-49
+#SBATCH --array=0-39
 ##SBATCH --partition=mcdermott
 ##SBATCH --partition=use-everything
 #SBATCH --exclude=node[001-030]
@@ -17,7 +17,7 @@ job_idx=$(($SLURM_ARRAY_TASK_ID + $offset))
 echo $(hostname)
 
 # OUTDIR_REGEX='/om2/user/msaddler/pitchnet/saved_models/models_sr20000/arch_0302/PND_v08_TLAS_snr_neg10pos10_AN_BW10eN1_IHC3000Hz_classification*'
-OUTDIR_REGEX='/om2/user/msaddler/pitchnet/saved_models/arch_search_v02_topN/sr20000_cf100_species002_spont070_BW*eN1_IHC3000Hz_IHC7order/arch_????'
+OUTDIR_REGEX='/om2/user/msaddler/pitchnet/saved_models/arch_search_v02_topN/[Ps]*/arch_????'
 # OUTDIR_REGEX='/om/scratch/*/msaddler/pitchnet/saved_models/arch_search_v01_spont070_BW10eN1_IHC3000Hz_IHC7order/arch*'
 # OUTDIR_REGEX='/om/scratch/*/msaddler/pitchnet/saved_models/arch_search_v02/arch*'
 # EFN_PREFIX='EVAL_SOFTMAX_TEST_*_ANMODEL_'
@@ -118,6 +118,34 @@ PRIOR_RANGE='0.5'
 # -j ${job_idx}
 
 
+# singularity exec --nv \
+# -B /home \
+# -B /om \
+# -B /nobackup \
+# -B /om2/user/msaddler \
+# -B /om4 \
+# /om2/user/msaddler/singularity-images/tfv1.13_unet.simg \
+# python /om2/user/msaddler/pitchnet/assets_psychophysics/f0dl_generalized.py \
+# -r "${OUTDIR_REGEX}/${EFN_PREFIX}testsnr_v02_bestckpt.json" \
+# -k 'snr_per_component' \
+# -p ${PRIOR_RANGE} \
+# -j ${job_idx}
+
+
+# singularity exec --nv \
+# -B /home \
+# -B /om \
+# -B /nobackup \
+# -B /om2/user/msaddler \
+# -B /om4 \
+# /om2/user/msaddler/singularity-images/tfv1.13_unet.simg \
+# python /om2/user/msaddler/pitchnet/assets_psychophysics/f0dl_generalized.py \
+# -r "${OUTDIR_REGEX}/${EFN_PREFIX}testspl_v02_bestckpt.json" \
+# -k 'dbspl' \
+# -p ${PRIOR_RANGE} \
+# -j ${job_idx}
+
+
 singularity exec --nv \
 -B /home \
 -B /om \
@@ -125,9 +153,8 @@ singularity exec --nv \
 -B /om2/user/msaddler \
 -B /om4 \
 /om2/user/msaddler/singularity-images/tfv1.13_unet.simg \
-python /om2/user/msaddler/pitchnet/assets_psychophysics/f0dl_generalized.py \
--r "${OUTDIR_REGEX}/${EFN_PREFIX}testsnr_v02_bestckpt.json" \
--k 'snr_per_component' \
+python /om2/user/msaddler/pitchnet/assets_psychophysics/f0dl_bernox.py \
+-r "${OUTDIR_REGEX}/${EFN_PREFIX}lowharm_v02_bestckpt.json" \
 -p ${PRIOR_RANGE} \
 -j ${job_idx}
 
@@ -139,9 +166,8 @@ singularity exec --nv \
 -B /om2/user/msaddler \
 -B /om4 \
 /om2/user/msaddler/singularity-images/tfv1.13_unet.simg \
-python /om2/user/msaddler/pitchnet/assets_psychophysics/f0dl_generalized.py \
--r "${OUTDIR_REGEX}/${EFN_PREFIX}testspl_v02_bestckpt.json" \
--k 'dbspl' \
+python /om2/user/msaddler/pitchnet/assets_psychophysics/f0dl_bernox.py \
+-r "${OUTDIR_REGEX}/${EFN_PREFIX}lowharm_v03_bestckpt.json" \
 -p ${PRIOR_RANGE} \
 -j ${job_idx}
 
