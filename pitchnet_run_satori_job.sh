a7bc4c6d9b3d710cca8e177e892420f6f98a15cc
Mark Saddler <msaddler@mit.edu>
2021-04-02 12:35:03 -0400

    measuring bernox2005/exact_v01 f0 discrimination thresholds for arch_search_v02_topN/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/arch_0??? networks

--- pitchnet_run_satori_job.sh
+++ pitchnet_run_satori_job.sh
@@ -41,4 +41,4 @@ parallel \
 --slf $SLURM_JOB_NODELIST_FILENAME \
 --joblog $SLURM_TASK_LOG_FILENAME \
 --wd $(pwd) \
-./pitchnet_run_satori.sh $PARALLEL_ARGUMENT_STRING ::: $(seq 0 19)
+./pitchnet_run_satori.sh $PARALLEL_ARGUMENT_STRING ::: $(seq 0 9)
