bb0f4c968d860a82a7b2cac9529a7834c929ee77
Mark Saddler <msaddler@mit.edu>
2021-04-09 15:59:29 -0400

    evaluating arch_search_v02_topN/cochlearn[_IHC4000Hz]_PND_v08????_noise_TLAS_snr_neg10pos10/arch_0??? networks on psychophysical stimuli (waveform tfrecords)

--- pitchnet_run_satori.sh
+++ pitchnet_run_satori.sh
@@ -129,52 +129,34 @@ OUTDIR=${outdir_list[$job_idx]}
 # DATA_EVAL='/data/PND_v08/noise_TLAS_snr_neg10pos10/'$DATA_TAG'/bez2018meanrates_0[8-9]*.tfrecords'
 # OUTPUT_LOG_FN=$OUTDIR'/output_train.log'
 
-## CHOOSE THE DATA_TAG BASED ON THE OUTPUT DIRECTORY
-DATA_TAG="PND_v08/noise_TLAS_snr_neg10pos10"
-if [[ "$OUTDIR" == *"PND_v08inst_noise_TLAS_snr_neg10pos10"* ]]; then
-  DATA_TAG="PND_v08inst/noise_TLAS_snr_neg10pos10"
-fi
-if [[ "$OUTDIR" == *"PND_v08spch_noise_TLAS_snr_neg10pos10"* ]]; then
-  DATA_TAG="PND_v08spch/noise_TLAS_snr_neg10pos10"
-fi
-if [[ "$OUTDIR" == *"noise_TLAS_snr_posInf"* ]]; then
-  DATA_TAG="PND_v08/noise_TLAS_snr_posInf"
-fi
-if [[ "$OUTDIR" == *"noise_TLAS_snr_pos10pos30"* ]]; then
-  DATA_TAG="PND_v08/noise_TLAS_snr_pos10pos30"
-fi
-if [[ "$OUTDIR" == *"noise_TLAS_snr_neg10pos10_filter_signalLPv01"* ]]; then
-  DATA_TAG="PND_v08/noise_TLAS_snr_neg10pos10_filter_signalLPv01"
-fi
-if [[ "$OUTDIR" == *"noise_TLAS_snr_neg10pos10_filter_signalHPv00"* ]]; then
-  DATA_TAG="PND_v08/noise_TLAS_snr_neg10pos10_filter_signalHPv00"
-fi
+# ## CHOOSE THE DATA_TAG BASED ON THE OUTPUT DIRECTORY
+# DATA_TAG="PND_v08/noise_TLAS_snr_neg10pos10"
+# if [[ "$OUTDIR" == *"PND_v08inst_noise_TLAS_snr_neg10pos10"* ]]; then
+#   DATA_TAG="PND_v08inst/noise_TLAS_snr_neg10pos10"
+# fi
+# if [[ "$OUTDIR" == *"PND_v08spch_noise_TLAS_snr_neg10pos10"* ]]; then
+#   DATA_TAG="PND_v08spch/noise_TLAS_snr_neg10pos10"
+# fi
+# if [[ "$OUTDIR" == *"noise_TLAS_snr_posInf"* ]]; then
+#   DATA_TAG="PND_v08/noise_TLAS_snr_posInf"
+# fi
+# if [[ "$OUTDIR" == *"noise_TLAS_snr_pos10pos30"* ]]; then
+#   DATA_TAG="PND_v08/noise_TLAS_snr_pos10pos30"
+# fi
+# if [[ "$OUTDIR" == *"noise_TLAS_snr_neg10pos10_filter_signalLPv01"* ]]; then
+#   DATA_TAG="PND_v08/noise_TLAS_snr_neg10pos10_filter_signalLPv01"
+# fi
+# if [[ "$OUTDIR" == *"noise_TLAS_snr_neg10pos10_filter_signalHPv00"* ]]; then
+#   DATA_TAG="PND_v08/noise_TLAS_snr_neg10pos10_filter_signalHPv00"
+# fi
 
-# DATA_TRAIN='/data/PND_v08/'$DATA_TAG'/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords'
-# DATA_EVAL='/data/PND_v08/'$DATA_TAG'/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords'
-DATA_TRAIN='/data/'$DATA_TAG'/train/*.tfrecords'
-DATA_EVAL='/data/'$DATA_TAG'/valid/*.tfrecords'
-OUTPUT_LOG_FN=$OUTDIR'/output_train.log'
+# # DATA_TRAIN='/data/PND_v08/'$DATA_TAG'/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[0-7]*.tfrecords'
+# # DATA_EVAL='/data/PND_v08/'$DATA_TAG'/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates_0[8-9]*.tfrecords'
+# DATA_TRAIN='/data/'$DATA_TAG'/train/*.tfrecords'
+# DATA_EVAL='/data/'$DATA_TAG'/valid/*.tfrecords'
+# OUTPUT_LOG_FN=$OUTDIR'/output_train.log'
 
-echo "[START TRAINING] $OUTPUT_LOG_FN" &> $(printf "slurm_run_train_satori-%04d.out" ${job_idx})
-export SINGULARITYENV_CUDA_VISIBLE_DEVICES
-singularity exec --nv \
--B $PATH_DATA:/data \
--B $PATH_SAVED_MODELS:/saved_models \
--B $PATH_CODE_LOCATION:/code_location \
--B $PATH_PYTHON_PACKAGES:/python-packages \
-docker://afrancl/ibm-hearing-aid-satori:tensorflow \
-./pitchnet_run_train.sh $OUTDIR $DATA_TRAIN $DATA_EVAL $OUTPUT_LOG_FN
-echo "[END TRAINING] $OUTPUT_LOG_FN" >> $(printf "slurm_run_train_satori-%04d.out" ${job_idx})
-
-
-# # OUTDIR=$(printf "/saved_models/arch_search_v02/arch_%04d" ${job_idx})
-# # DATA_TAG="sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order"
-# TFRECORDS_REGEX="$DATA_TAG/*.tfrecords"
-# EFN_PREFIX='EVAL_SOFTMAX_'
-# OUTPUT_LOG_FN=$OUTDIR'/output_eval.log'
-
-# echo "[START EVALUATION] $OUTPUT_LOG_FN" &> $(printf "slurm_run_eval_satori-%04d.out" ${job_idx})
+# echo "[START TRAINING] $OUTPUT_LOG_FN" &> $(printf "slurm_run_train_satori-%04d.out" ${job_idx})
 # export SINGULARITYENV_CUDA_VISIBLE_DEVICES
 # singularity exec --nv \
 # -B $PATH_DATA:/data \
@@ -182,5 +164,24 @@ echo "[END TRAINING] $OUTPUT_LOG_FN" >> $(printf "slurm_run_train_satori-%04d.ou
 # -B $PATH_CODE_LOCATION:/code_location \
 # -B $PATH_PYTHON_PACKAGES:/python-packages \
 # docker://afrancl/ibm-hearing-aid-satori:tensorflow \
-# ./pitchnet_run_eval.sh $OUTDIR $TFRECORDS_REGEX $EFN_PREFIX $OUTPUT_LOG_FN
-# echo "[END EVALUATION] $OUTPUT_LOG_FN" >> $(printf "slurm_run_eval_satori-%04d.out" ${job_idx})
+# ./pitchnet_run_train.sh $OUTDIR $DATA_TRAIN $DATA_EVAL $OUTPUT_LOG_FN
+# echo "[END TRAINING] $OUTPUT_LOG_FN" >> $(printf "slurm_run_train_satori-%04d.out" ${job_idx})
+
+
+# OUTDIR=$(printf "/saved_models/arch_search_v02/arch_%04d" ${job_idx})
+# DATA_TAG="sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order"
+# TFRECORDS_REGEX="$DATA_TAG/*.tfrecords"
+TFRECORDS_REGEX="stim_waveform.tfrecords"
+EFN_PREFIX='EVAL_SOFTMAX_'
+OUTPUT_LOG_FN=$OUTDIR'/output_eval.log'
+
+echo "[START EVALUATION] $OUTPUT_LOG_FN" &> $(printf "slurm_run_eval_satori-%04d.out" ${job_idx})
+export SINGULARITYENV_CUDA_VISIBLE_DEVICES
+singularity exec --nv \
+-B $PATH_DATA:/data \
+-B $PATH_SAVED_MODELS:/saved_models \
+-B $PATH_CODE_LOCATION:/code_location \
+-B $PATH_PYTHON_PACKAGES:/python-packages \
+docker://afrancl/ibm-hearing-aid-satori:tensorflow \
+./pitchnet_run_eval.sh $OUTDIR $TFRECORDS_REGEX $EFN_PREFIX $OUTPUT_LOG_FN
+echo "[END EVALUATION] $OUTPUT_LOG_FN" >> $(printf "slurm_run_eval_satori-%04d.out" ${job_idx})
