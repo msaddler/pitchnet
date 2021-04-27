2d9517941cc6f24a094ba1f085736243ab440df3
Mark Saddler <msaddler@mit.edu>
2021-04-27 18:52:13 -0400

    generating /data_pitchnet/bernox2005/neurophysiology_v02_inharmonic_EqualAmpTEN_lharm01to15_phase0_f0min080_f0max640/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates tfrecords

--- assets_datasets/tfrecords_from_hdf5_job.sh
+++ assets_datasets/tfrecords_from_hdf5_job.sh
@@ -16,7 +16,7 @@
 # source_regex='/om/user/msaddler/data_pitchnet/bernox2005/neurophysiology_SlidingFixedFilter_lharm01to30_phase0_f0min080_f0max320/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/*.hdf5'
 # source_regex="$SCRATCH_PATH"'/data_hearinglossnet/pitchrepnet_eval2afc_bernox2005/sr10000_cf050_cohc10eN1_cihc10eN1_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
 # source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order_cohc0_dBSPL60to90/bez2018meanrates*.hdf5'
-source_regex='/om/user/msaddler/data_pitchnet/bernox2005/lowharm_v01_thresh40/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
+source_regex='/om/user/msaddler/data_pitchnet/bernox2005/neurophysiology_v02_inharmonic_EqualAmpTEN_lharm01to15_phase0_f0min080_f0max640/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
 
 # source_regex='/om/user/msaddler/data_pitchnet/*/*/stim_waveform.hdf5'
 
