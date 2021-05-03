0508f71ff4559cd68ccfc93f1af99c1130d69624
Mark Saddler <msaddler@mit.edu>
2021-05-03 19:51:28 -0400

    generating data_pitchnet/bernox2005/puretone_v01/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates nervegrams

--- assets_datasets/nervegram_run_parallel_job.sh
+++ assets_datasets/nervegram_run_parallel_job.sh
@@ -18,7 +18,7 @@
 # source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/*.hdf5'
 # jobs_per_source_file=3
 
-source_regex='/om/user/msaddler/data_pitchnet/bernox2005/neurophysiology_v02_inharmonic_fixed_EqualAmpTEN_lharm01to15_phase0_f0min080_f0max640/stim.hdf5'
+source_regex='/om/user/msaddler/data_pitchnet/bernox2005/puretone_v01/stim.hdf5'
 jobs_per_source_file=15
 
 offset=0
