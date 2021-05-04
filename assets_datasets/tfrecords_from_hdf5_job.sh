ab01abc4059f7229b11e07d6f8582098403fcce1
Mark Saddler <msaddler@mit.edu>
2021-05-03 20:24:39 -0400

    generating data_pitchnet/bernox2005/puretone_v0[23]/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates tfrecords

--- assets_datasets/tfrecords_from_hdf5_job.sh
+++ assets_datasets/tfrecords_from_hdf5_job.sh
@@ -6,17 +6,17 @@
 #SBATCH --mem=2500
 #SBATCH --nodes=1
 #SBATCH --time=0-4:00:00
-#SBATCH --array=0-14
-#SBATCH --partition=mcdermott
+#SBATCH --array=0-29
+##SBATCH --partition=mcdermott
 ##SBATCH --partition=use-everything
-#SBATCH --dependency=afterok:20028359
+#SBATCH --dependency=afterok:20028382
 #SBATCH --exclude=node[001-030]
 
 ### Define source_regex and dest_filename here (use single quotes to prevent regex from expanding)
 # source_regex='/om/user/msaddler/data_pitchnet/bernox2005/neurophysiology_SlidingFixedFilter_lharm01to30_phase0_f0min080_f0max320/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/*.hdf5'
 # source_regex="$SCRATCH_PATH"'/data_hearinglossnet/pitchrepnet_eval2afc_bernox2005/sr10000_cf050_cohc10eN1_cihc10eN1_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
 # source_regex="$SCRATCH_PATH"'/data_pitchnet/PND_v08/noise_TLAS_snr_neg10pos10/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order_cohc0_dBSPL60to90/bez2018meanrates*.hdf5'
-source_regex='/om/user/msaddler/data_pitchnet/bernox2005/puretone_v01/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
+source_regex='/om/user/msaddler/data_pitchnet/bernox2005/puretone_v0[23]/sr20000_cf100_species002_spont070_BW10eN1_IHC3000Hz_IHC7order/bez2018meanrates*.hdf5'
 
 # source_regex='/om/user/msaddler/data_pitchnet/*/*/stim_waveform.hdf5'
 
